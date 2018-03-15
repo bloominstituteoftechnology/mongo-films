39437f6e5fcb2dae96ff6cbd362e31ed6416abe4
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-03-15 18:24:30 -0500

    Initial Commit
    download dependencies
    delete yarn.lock
    in Film.js:
    link starships, vehicles, planets, characters, and species to schema
    in filmsRouter.js:
    add GET route to search films by producer name in query (incomplete)

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -3,7 +3,18 @@ const express = require('express');
 const Film = require('./Film.js');
 
 const router = express.Router();
-
-// add endpoints here
+router.get('/', (req, res) => {
+  const producerFilter = true;
+    let query = Film.find({})
+      .select('title producer')
+      .where({ producer: /gary kurtz/i })
+      .sort('episode')
+      .exec((err, answer) => {
+        res.status(200).json({ answer: answer });
+      });
+});
 
 module.exports = router;
+
+// .populate('characters', 'name gender height skin_color')
+// .populate('planets', 'name climate terrain gravity diameter')
