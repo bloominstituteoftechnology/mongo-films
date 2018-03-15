1999a85917b6c810c3f4045e4e5abb32f1ab8535
temp <temp@Debis-Home-Mac.local>
2018-03-15 15:48:09 -0400

    Got the producer search in filmsRouter.js working with RegEx.

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -8,15 +8,13 @@ const router = express.Router();
 router.get('/', function(req, res) {
   const producerFilter = req.query.producer;
 
-  let query = Film.find({})
-    .sort('episode')
-    // .populate('characters planets species')
-    .select('title producer');
+  let query = Film.find({}).sort('episode');
   // .populate('characters', 'name gender height skin_color')
   // .populate('planets', 'name climate terrain gravity diameter');
 
   if (producerFilter) {
-    query.where({ producer: /gary kurtz/i });
+    const producerRegx = new RegExp(producerFilter, 'i');
+    query.select('title producer').where({ producer: producerRegx });
   }
 
   query.then(films => {
