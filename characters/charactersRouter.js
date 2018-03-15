0c504aafa30fee6fe0486644666bfac60e377335
ashleijo <shuryjo@gmail.com>
2018-03-15 15:38:21 -0400

    adding routes to Film based of lecture
    also adding routes for Character to get list of characters and also a route for each character's _id as params

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
 const Character = require('./Character.js');
-
 const router = express.Router();
 
-// add endpoints here
+router.get('/', function(req,res) {
+  Character.find({})
+  .sort('key')
+  .select('name')
+  .then(characters => {
+    res.json(characters);
+  })
+})
+
+router.get('/:_id', function(req,res) {
+	const id = req.params._id;
+  Character.findOne({_id: id})
+  .then(character => {
+    res.json(character);
+  })
+})
 
 module.exports = router;
