1f2fa4866dda0721d1282aa065aca48710ac6363
Nate152 <35231160+Nate152@users.noreply.github.com>
2018-03-15 18:04:55 -0400

    First Commit, Films route complete, working on rest

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,9 +1,39 @@
 const express = require('express');
-
+const Film = require('../films/Film.js');
 const Character = require('./Character.js');
+const Vehicle = require('../vehicles/Vehicle.js');
 
 const router = express.Router();
 
 // add endpoints here
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    
+    let query = Character.findById(id)
+    .populate('homeworld')
+    .then(character => {
+        res.json(character)
+    })
+    .catch(err => {
+        res.err(err)
+    })
+    /*let query = Character.where({ key: id })
+    query.findOne((err, character) => {
+        if (err) return handleError(err);
+        if (character)
+    })
+    .populate('homeworld');
+
+     appearedIn = Film.find({})
+    .populate('characters')
+    .select('key')
+    
+    
+    query.then(character => {
+        res.json(character);
+    }) */
+})
+
 
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,24 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
+    const producerFilter = req.query.producer;
+    const releasedFilter = req.query.released;
+
+    let query = Film.find({})
+        .sort('episode')
+        .populate('characters', 'name gender height skin_color hair_color eye_color')
+        .populate('planets', 'name climate terrain gravity diameter');
+        //.select('title producer');
+    if (producerFilter) {
+        query.where({ producer: /gary kurtz/i });
+    }
+    if (releasedFilter) {
+        query.where({ release_date: /2005/i });
+    }
+    query.then(films => {
+        res.json(films);
+    });
+});
 
 module.exports = router;
