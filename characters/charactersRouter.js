50aee95fabe62f44347858163bf18b5bf09e6ce3
Dani Tacheny <dani.tacheny@gmail.com>
2018-03-15 17:45:55 -0500

    add GET characters over 100cm and female by minheight query

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -13,41 +13,59 @@ router.get('/:id', (req, res) => {
 
   Character.findById(id)
     .populate('homeworld')
-    .populate('movies')
     .then(char => {
       let key = char.key;
-      Film.find({ character_ids: key })
+      Film.find({character_ids: key})
         .then(films => {
           char.movies = films;
         })
         .then(() => {
           res.status(200).json(char);
         })
-				.catch(err => {
-					res.status(500).json({ msg: 'Error getting the films for the character' });
-				})
+        .catch(err => {
+          res
+            .status(500)
+            .json({msg: 'Error getting the films for the character'});
+        });
     })
     .catch(err => {
-      res.status(500).json({ msg: 'Error getting character by that ID' });
+      res.status(500).json({msg: 'Error getting character by that ID'});
     });
 });
 
 router.get('/:id/vehicles', (req, res) => {
-	const id = req.params.id;
-	Character.findById(id)
-		.then(char => {
-			const key = char.key;
-			Vehicle.find({ pilot_keys: key })
-				.then(vehicles => {
-					res.status(200).json(vehicles);
-				})
-				.catch(err => {
-					res.status(500).json({ msg: 'Error getting the vehicles' });
-				})
-		})
+  const id = req.params.id;
+  Character.findById(id)
+    .then(char => {
+      const key = char.key;
+      Vehicle.find({pilot_keys: key})
+        .then(vehicles => {
+          res.status(200).json(vehicles);
+        })
+        .catch(err => {
+          res.status(500).json({msg: 'Error getting the vehicles'});
+        });
+    })
+    .catch(err => {
+      res.status(500).json({msg: 'Error getting character by that ID'});
+    });
+});
+
+router.get('/', (req, res) => {
+	const heightQuery = req.query.minheight;
+
+	const query = Character.find({})
+
+	if (heightQuery) {
+		console.log(heightQuery);
+		query.find({ height: { $gt: Number(heightQuery) }, gender: 'female' });
+	}
+
+	query.then(chars => {
+		res.status(200).json(chars);
+	})
 		.catch(err => {
-			res.status(500).json({ msg: 'Error getting character by that ID' });
+			res.status(500).json({ msg: 'Error getting characters' });
 		})
 })
-
 module.exports = router;
