ac85fd5744c787dbe1c5a51d2c684ad44c4f4ffc
ashleijo <shuryjo@gmail.com>
2018-03-15 18:32:02 -0400

    adding to vehicle route/schema so that character/vehicle route returns the character name and their vehicles

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const Character = require('./Character.js');
 const Film = require('../films/Film.js');
+const Vehicle = require('../vehicles/Vehicle.js');
 const router = express.Router();
 
 router.get('/', function(req,res) {
   Character.find({})
   .sort('key')
-  .populate('homeworld')
   .then(characters => {
     res.json(characters);
   })
@@ -15,20 +15,24 @@ router.get('/', function(req,res) {
 router.get('/:_id', function(req,res) {
 	const id = req.params._id;
   Character.findOne({_id: id})
-  .select('name gender height homeworld')
+  .select('name gender height homeworld movies')
   .populate('homeworld')
   .then(character => {
     Film.find({characters : id })
     .select('title')
-    .then(p => res.status(200).json([character,p]))
+    .then(movies => res.json({"character": character, "movies": movies}))
   })
+})
 
-
-
-
-  /*.then(character => {
-    res.json(character);
-  })*/
+router.get('/:_id/vehicles', function(req,res) {
+	const id = req.params._id;
+  Character.findOne({_id: id})
+  .select('name')
+  .then(character => {
+    Vehicle.find({pilots : id})
+    .select('vehicle_class')
+    .then(vehicles => res.json({"character": character, "vehicles": vehicles}))
+  })
 })
 
 module.exports = router;
