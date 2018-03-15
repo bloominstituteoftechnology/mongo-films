45c6886bee1874af22c67b5968258a7f267d431b
Mephestys <mephestys@gmail.com>
2018-03-15 12:42:49 -0600

    Initial commit

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,6 +4,26 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
-// add endpoints here
+router.get('/', function(req, res) {
+  const producerFilter = new RegExp(req.query.producer, 'i');
+
+  let query = Film.find({})
+  .sort('episode')
+  .select('title producer')
+  // .populate('characters', 'name gender height skin_color')
+  // .populate('planets', 'name climate terrain gravity diameter')
+  // .then(films => {
+  //   res.status(200).json(films)
+  // })
+  // .catch(err => res.status(500).json(err));
+
+  if (producerFilter) {
+    query.where({ producer: producerFilter });
+  }
+
+  query.then(films => {
+    res.json(films);
+  });
+});
 
 module.exports = router;
