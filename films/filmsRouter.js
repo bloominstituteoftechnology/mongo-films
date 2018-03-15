ff68006287e574532e3af55bf2b5306e1e61f543
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-15 14:18:31 -0400

    film get endpoint

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,6 +4,22 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
-// add endpoints here
+router.get('/', (req, res) => {
+  const { producerFilter } = req.query.producer;
+  let query = Film.find({})
+    .sort('episode')
+    .select('title producer')
+    // .populate('characters', 'name gender height skin_color')
+    // .populate('planets', 'name climate terrain gravity diameter')
+  if (producerFilter) {
+    query.where({ producer: /producerFilter/i })
+  }
+  query.then(films => {
+    res.status(200).json(films);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+});
 
 module.exports = router;
