d10394d076d3568f02a2d254b011e2db4b11cdf0
TERR1E <terrieb.kim@gmail.com>
2018-03-15 11:59:36 -0700

    initial commit, example from Luis included

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,22 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', function(req, res) {
+  const producerFilter = req.query.producer;
+
+  let query = Film.find({})
+    .sort('episode')
+    // .populate('characters planets species')
+    .select('title producer')
+    // .populate('chacters', 'name gender height skin_color')
+    // .populate('planets', 'name climate terrain gravity diameter')
+  if (producerFilter) {
+    query.where({ producer: /gary kurtz/i });
+  }
+
+  query.then(films => {
+    res.json(films);
+  });
+});
 
 module.exports = router;
