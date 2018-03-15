44aaed86d6478690e0fb4593bc2c693ef1cde899
Dani Tacheny <dani.tacheny@gmail.com>
2018-03-15 16:15:03 -0500

    GET character's vehicles now working

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const Character = require('./Character.js');
 const Film = require('../films/Film.js');
+const Vehicle = require('../vehicles/Vehicle.js');
 
 const router = express.Router();
 
@@ -17,16 +18,36 @@ router.get('/:id', (req, res) => {
       let key = char.key;
       Film.find({ character_ids: key })
         .then(films => {
-          console.log(films);
           char.movies = films;
         })
         .then(() => {
           res.status(200).json(char);
-        });
+        })
+				.catch(err => {
+					res.status(500).json({ msg: 'Error getting the films for the character' });
+				})
     })
     .catch(err => {
       res.status(500).json({ msg: 'Error getting character by that ID' });
     });
 });
+
+router.get('/:id/vehicles', (req, res) => {
+	const id = req.params.id;
+	Character.findById(id)
+		.then(char => {
+			const key = char.key;
+			Vehicle.find({ pilot_keys: key })
+				.then(vehicles => {
+					res.status(200).json(vehicles);
+				})
+				.catch(err => {
+					res.status(500).json({ msg: 'Error getting the vehicles' });
+				})
+		})
+		.catch(err => {
+			res.status(500).json({ msg: 'Error getting character by that ID' });
+		})
+})
 
 module.exports = router;
