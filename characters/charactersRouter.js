3a4ea5421180d1f0e35c86b2562d62803d2b7595
aceredesign <steven.magadan@hotmail.com>
2018-03-15 14:31:54 -0700

    Now working on female height

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -25,16 +25,24 @@ router.get("/:id", (req, res) => {
 
 router.get("/:id/vehicles", (req, res) => {
   const characterId = req.params.id;
-  let charFinder = Character.findById(characterId).populate("vehicle_class");
+  let charFinder = Character.findById(characterId);
 
-  charFinder.then(character => {
-    res.json(character);
-  });
-  charFinder.catch(err => {
-    res
-      .status(500)
-      .json({ message: "There was an error finding the character" });
-  });
+  charFinder
+    .then(character => {
+      const key = character.key;
+      Vehicle.find({ pilot_keys: key })
+        .then(vehicle => {
+          res.status(200).json(vehicle);
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ message: "There was an error finding the vehicle" });
+        });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "There was an error finding the ID" });
+    });
 });
 
 module.exports = router;
