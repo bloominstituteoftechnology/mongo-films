c45c2f5dbf67cf2957978d419ae8db807d613c75
Ronnie Miksch <fakepreferences@gmail.com>
2018-03-15 11:48:26 -0700

    Class work and character id get complete

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,5 +5,17 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/:id',  (req, res) => {
+  const charId = req.params.id;
+
+  Character.findById(charId)
+    .populate('homeworld')
+    .then(char => {
+      res.send(char);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    })
+});
 
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,30 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
+  const producerFilter = req.query.producer;
+  const releaseFilter = req.query.released;
+
+  let query = Film.find()
+    .sort('episode')
+    .populate('characters', 'name gender height skin_color hair_color eye_color')
+    .populate('planets', 'name climate terrain gravity diameter')
+
+    if(producerFilter) {
+      const filter = new RegExp(producerFilter, 'i');
+      query.where({ producer: filter });
+    }
+    if(releaseFilter) {
+      const filter = new RegExp(releaseFilter, 'i');
+      query.where({ release_date: filter });
+    }
+    query.then(films => {
+      res.send(films);
+    })
+    .catch(err => {
+      res.status(400).send({ error: err });
+    });
+
+});
 
 module.exports = router;
