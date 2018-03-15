12b272f1b356ec5ae384d5f2e0a5b84852a535d7
TERR1E <terrieb.kim@gmail.com>
2018-03-15 12:51:35 -0700

    GET list of all films done

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,22 +5,19 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
-router.get('/', function(req, res) {
-  const producerFilter = req.query.producer;
-
-  let query = Film.find({})
+router.get('/', (req, res) => {
+  Film.find({})
     .sort('episode')
-    // .populate('characters planets species')
-    .select('title producer')
-    // .populate('chacters', 'name gender height skin_color')
-    // .populate('planets', 'name climate terrain gravity diameter')
-  if (producerFilter) {
-    query.where({ producer: /gary kurtz/i });
-  }
-
-  query.then(films => {
-    res.json(films);
-  });
+    .populate('planets', 'name climate terrain gravity diameter')
+    .populate('characters', 'name gender height skin_color hair_color eye_color')
+    .exec()
+    .then(films => {
+      res.status(200).json(films);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: 'There was an error while retrieving the films from the database.' });
+    })
 });
 
 module.exports = router;
