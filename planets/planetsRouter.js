98ea8447f0c516e0f48eca67d6fd672a63515761
ashleijo <shuryjo@gmail.com>
2018-03-15 18:57:55 -0400

    adding planet route that also returns characters born on that planet

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
 const Planet = require('./Planet.js');
-
+const Character = require('../characters/Character.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/:_id', function(req,res) {
+  const id = req.params._id;
+  Planet.findOne({_id: id})
+  .select('name')
+  .then(planet => {
+    //res.json({"planet": planet});
+    Character.find({homeworld: id})
+    .select('name')
+    .then(characters => res.json({"planet": planet, "characters": characters}))
+  })
+})
 
 module.exports = router;
