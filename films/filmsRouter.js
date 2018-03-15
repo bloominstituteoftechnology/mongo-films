5b85b4602697df4690fc1b1d33d805d69f239bfa
TERR1E <terrieb.kim@gmail.com>
2018-03-15 13:49:48 -0700

    GET by director and release date complete

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,18 +6,45 @@ const router = express.Router();
 
 // add endpoints here
 router.get('/', (req, res) => {
+  const producerFilter = req.query.producer;
+  const releaseFilter = req.query.released;
+
+  let query = Film.find({}).sort('episode');
+
   Film.find({})
     .sort('episode')
     .populate('planets', 'name climate terrain gravity diameter')
     .populate('characters', 'name gender height skin_color hair_color eye_color')
     .exec()
-    .then(films => {
+      if (producerFilter) {
+        query.select('title producer').where({ producer: /gary kurtz/i });
+      } else if (releaseFilter) {
+        query.select('title release_date').where({ release_date: /2005/ });
+      }
+
+    query.then(films => {
       res.status(200).json(films);
     })
     .catch(error => {
       console.log(error);
       res.status(500).json({ error: 'There was an error while retrieving the films from the database.' });
-    })
+    });
 });
+
+// router.get('/', function(req, res) {
+//   const producerFilter = req.query.producer;
+//   const releaseFilter = req.query.released;
+
+//   let query = Film.find({}).sort('episode');
+//   if (producerFilter) {
+//     query.select('title producer').where({ producer: /gary kurtz/i });
+//   } else if (releaseFilter) {
+//     query.select('title release_date').where({ release_date: /2005/ });
+//   }
+
+//   query.then(films => {
+//     res.json(films);
+//   });
+// });
 
 module.exports = router;
