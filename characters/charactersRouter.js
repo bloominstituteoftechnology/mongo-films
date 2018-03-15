69daa08147ec33b697a8de37cf0731e275e2d77e
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-15 18:52:45 -0400

    CharacterRouter working by ID

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -8,29 +8,13 @@ const router = express.Router();
 router.get('/:id', (req, res) => {
   const { id } = req.params;
 
-  // const query = Character.findById(id)
-  // .populate('homeworld')
-  // .populate('films')
-  // .then(character => res.status(200).json(character));
   Character.findById(id)
-// .populate('homeworld')
-// .populate('movies')
-.then(char => {
-let key = char.key
-Film.find({character_ids:key})
-  .then(films => char.movies = films)
-  .then(()=> res.status(200).json(char))
-  })
+  .then(char => {
+    Film.find({ character_ids: char.key })
+      .populate('characters','name')
+      .then(films => (char.movies = films))
+      .then(films => res.status(200).json(char));
+  });
 });
 
 module.exports = router;
-
-Character.findById(id)
-// .populate('homeworld')
-// .populate('movies')
-.then(char => {
-let key = char.key
-Film.find({character_ids:key})
-  .then(films => char.movies = films)
-  .then(()=> res.status(200).json(char))
-  })
