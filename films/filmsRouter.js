405f45de40a577e60d4476b95f71dd07acb7519b
temp <temp@Debis-Home-Mac.local>
2018-03-15 18:05:12 -0400

    Finished everything except for the final problem, the pseudo-stretch.

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -7,14 +7,20 @@ const router = express.Router();
 // add endpoints here
 router.get('/', function(req, res) {
   const producerFilter = req.query.producer;
+  const filmReleased = req.query.released;
 
-  let query = Film.find({}).sort('episode');
-  // .populate('characters', 'name gender height skin_color')
-  // .populate('planets', 'name climate terrain gravity diameter');
+  let query = Film.find({}).sort('episode')
+  .populate('characters', 'name gender height skin_color')
+  .populate('planets', 'name climate terrain gravity diameter');
 
   if (producerFilter) {
     const producerRegx = new RegExp(producerFilter, 'i');
-    query.select('title producer').where({ producer: producerRegx });
+    query.where({ producer: producerRegx });
+  }
+
+  if (filmReleased) {
+    const filmRegx = new RegExp(filmReleased);
+    query.where({ release_date: filmRegx });
   }
 
   query.then(films => {
