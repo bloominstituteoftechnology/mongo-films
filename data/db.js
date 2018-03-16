59ec921c17350744c00de6b60433d2c0bc60b987
german gonzalez <gcomlnk@gmail.com>
2018-03-16 01:51:41 -0400

    importing database and setting up connection to mongo

--- data/db.js
+++ data/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 module.exports = {
-    connectTo: function (database = 'sandbox', host = 'localhost') {
-        return mongoose.connect(`mongodb://${host}/${database}`);
+    connectTo: function (database = 'starwars', host = 'germancin:secure123@167.99.10.46') {
+        return mongoose.connect(`mongodb://${host}/${database}`)
+                .then(conn => console.log(`Connected to MongoDB - Server:167.99.10.46 DB:${database}`))
+                .catch(err => console.log('error :::: ' + err));
     },
 };
+
+
