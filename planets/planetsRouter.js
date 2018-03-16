199178ba81d8a4638e527b50e922f9f9d44cac3c
Mephestys <mephestys@gmail.com>
2018-03-15 18:48:52 -0600

    Full implementation of character/species homeworld search

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -9,16 +9,12 @@ const router = express.Router();
 router.get('/:id', function(req, res) {
   const { id } = req.params;
 
-  Planet.findOne({ key: id })
-    .select('name climate surface_water terrain gravity orbital_period diameter')
-    .then(char => {
-      Character.find({ homeworld_key: id })
-        .select('name')
-        .then(chr => {
-          const character = { ...chr._doc, characters: chr };
-          res.json(character);
-        });
-    });
+  const findCharacter = Character.find({ homeworld_key: id }).select('name');
+  const findSpecies = Species.find({ homeworld_key: id }).select('name');
+
+  Promise.all([findCharacter, findSpecies]).then(data => {
+    res.json({ characters: data[0], species: data[1] });
   });
+});
 
 module.exports = router;
