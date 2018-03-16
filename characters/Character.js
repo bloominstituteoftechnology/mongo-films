71c28503267ea75f473a8e6fc9b7433ab822c48c
german gonzalez <gcomlnk@gmail.com>
2018-03-16 01:26:44 -0400

    reformating spaces

--- characters/Character.js
+++ characters/Character.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const Character = mongoose.Schema({
-  name: { type: String, required: true },
-  edited: Date,
-  created: Date,
-  gender: String,
-  height: String,
-  hair_color: String,
-  skin_color: String,
-  eye_color: String,
-  birth_year: String,
-  key: { type: Number, unique: true },
-  homeworld_key: Number,
-  // add homeworld field that links the character to it's planet
+    name: {type: String, required: true},
+    edited: Date,
+    created: Date,
+    gender: String,
+    height: String,
+    hair_color: String,
+    skin_color: String,
+    eye_color: String,
+    birth_year: String,
+    key: {type: Number, unique: true},
+    homeworld_key: Number,
+    // add homeworld field that links the character to it's planet
 });
 
 module.exports = mongoose.model('Character', Character);
--- planets/Planet.js
+++ planets/Planet.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const Planet = new mongoose.Schema({
-  edited: { type: Date, default: Date.now },
-  climate: String,
-  surface_water: String,
-  name: { type: String, required: true, index: true },
-  diameter: String,
-  rotation_period: String,
-  created: { type: Date, default: Date.now },
-  terrain: String,
-  gravity: String,
-  orbital_period: String,
-  key: { type: Number, unique: true },
+    edited: {type: Date, default: Date.now},
+    climate: String,
+    surface_water: String,
+    name: {type: String, required: true, index: true},
+    diameter: String,
+    rotation_period: String,
+    created: {type: Date, default: Date.now},
+    terrain: String,
+    gravity: String,
+    orbital_period: String,
+    key: {type: Number, unique: true},
 });
 
 module.exports = mongoose.model('Planet', Planet);
