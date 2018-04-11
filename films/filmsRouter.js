26ea3ef0ffd38074b25fdbb85945f8b065f6e586
habib1234731 <habibrehman1234731@gmail.com>
2018-04-11 18:02:24 -0400

    search films by date and producer working

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -9,16 +9,24 @@ const router = express.Router();
 router
   .route('/')
   .get((req, res) => {
+    const producerFilter = new RegExp(req.query.producer, 'i');
+    const releasedFilter = new RegExp(req.query.released, 'i');
     const query = Film.find({})
     .sort('episode')
     .populate('characters', 'name gender height skin_color hair_color eye_color')
     .populate('planets', 'name climate terrain gravity diameter')
-      .then(films => {
-        res.status(200).json(films);
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
-  })
+    if(req.query.producer) {
+        query.where({producer: producerFilter});
+    }
+    if(req.query.released) {
+        query.where({release_date: releasedFilter})
+    }
+    query.then(films => {
+    res.status(200).json(films);
+    })
+    .catch(err => {
+    res.status(500).json(err);
+    });
+  })    
 
 module.exports = router;
