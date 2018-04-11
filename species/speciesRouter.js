c64f7b3c55d68dd0ef0f4f452310a5d5c08f9b56
astro11 <aquila.strother@asu.edu>
2018-04-11 16:40:15 -0400

    added GET endpoints for rest of collections

--- species/speciesRouter.js
+++ species/speciesRouter.js
@@ -5,5 +5,17 @@ const Specie = require('./Specie.js');
 const router = express.Router();
 
 // add endpoints here
-
+router
+    .route('/')
+    .get((req, res) => {    
+      Film.find({})   
+        .then(films => {
+          res.send(films);
+        })
+        .catch(err => {
+          res.status(500).send({ error: err });
+        });
+    
+    });
+    
 module.exports = router;
--- starships/starshipsRouter.js
+++ starships/starshipsRouter.js
@@ -5,5 +5,17 @@ const Starship = require('./Starship.js');
 const router = express.Router();
 
 // add endpoints here
-
+router
+    .route('/')
+    .get((req, res) => {    
+      Film.find({})   
+        .then(films => {
+          res.send(films);
+        })
+        .catch(err => {
+          res.status(500).send({ error: err });
+        });
+    
+    });
+    
 module.exports = router;
