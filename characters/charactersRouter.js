b46078294b47b492daf80afb6bb713e123e31b29
AJ Genung <DragonRydrz@me.com>
2018-04-11 15:34:59 -0400

    characters get working

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -4,6 +4,21 @@ const Character = require('./Character.js');
 
 const router = express.Router();
 
+const serverError = error => {
+  res.status(500).json(error);
+};
+
 // add endpoints here
+
+router
+  .route('/')
+  .get((req, res) => {
+    Character.find()
+      .then(characters => {
+        res.json(characters);
+      })
+      .catch(serverError);
+  })
+  .post();
 
 module.exports = router;
