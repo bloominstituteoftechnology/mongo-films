3a99f3e526c7bdba22b3bff889e145c78fdbaf73
Johnathan Huggett <johnathanhuggett@Johnathans-MacBook-Pro.local>
2018-04-11 18:41:39 -0400

    Johnathan Huggett, working on character

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
 
-const Character = require('./Character.js');
+const Character = require("./Character.js");
 
 const router = express.Router();
 
-// add endpoints here
+// api/character
+
+router.route("/:id").get((req, res) => {
+    Character.findById(req.params.id)
+        .then(character => {})
+        .catch(err => res.status(500).json(err));
+});
 
 module.exports = router;
--- films/Film.js
+++ films/Film.js
@@ -1,23 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
+const Starship = require("../starships/Starship");
+const Vehicles = require("../vehicles/Vehicle");
+const Planets = require("../planets/Planet");
+const Characters = require("../characters/Character");
+const Species = require("../species/Specie");
+
 const Film = new mongoose.Schema({
-  created: { type: Date, default: Date.now },
-  episode: Number,
-  edited: { type: Date, default: Date.now },
-  planet_ids: [Number],
-  producer: String,
-  title: { type: String, required: true },
-  director: String,
-  release_date: String,
-  opening_crawl: String,
-  character_ids: [Number],
-  specie_ids: [Number],
-  key: { type: Number, unique: true },
-  starship_ids: [Number],
-  vehicle_ids: [Number],
-  // add fields for starships, vehicles, planets, characters and species
-  // to link them to the corresponding model
+    created: { type: Date, default: Date.now },
+    episode: Number,
+    edited: { type: Date, default: Date.now },
+    planet_ids: [Number],
+    producer: String,
+    title: { type: String, required: true },
+    director: String,
+    release_date: String,
+    opening_crawl: String,
+    character_ids: [Number],
+    specie_ids: [Number],
+    key: { type: Number, unique: true },
+    starship_ids: [Number],
+    vehicle_ids: [Number],
+    // add fields for starships, vehicles, planets, characters and species
+    // to link them to the corresponding model
+    starships: [{ type: ObjectId, ref: "Starship" }],
+    vehicles: [{ type: ObjectId, ref: "Vehicle" }],
+    planets: [{ type: ObjectId, ref: "Planet" }],
+    characters: [{ type: ObjectId, ref: "Character" }],
+    species: [{ type: ObjectId, ref: "Specie" }],
 });
 
-module.exports = mongoose.model('Film', Film);
+module.exports = mongoose.model("Film", Film);
