ce4b0af5127c5382618c7804cb0a75a6e7050074
Justin Kaseman <justinkaseman@live.com>
2018-04-11 15:45:53 -0700

    4pm commit - finish get all films

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,6 +4,28 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
-// add endpoints here
+router.get('/', (req, res) => {
+	const parameters = req.query;
+	console.log(parameters);
+	if (parameters.producer) {
+		Film.find({})
+			.select({ producer: parameters.producer })
+			.then(films => {
+				res.status(200).json(films);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	} else {
+		Film.find({})
+			.sort({ episode: 'asc' })
+			.then(films => {
+				res.status(200).json(films);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
+});
 
 module.exports = router;
