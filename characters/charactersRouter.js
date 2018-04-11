beadd53676782bad74cf5206e29ee7acd21b88b0
Cesar Mejia Leiva <cmejia@gmail.com>
2018-04-11 13:34:16 -0700

    linked all schemas; get/post characters/film endpoints implemented

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,5 +5,27 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
+router
+  .route('/')
+  .get((req, res) => {
+    Character.find({})
+      .then(chars => {
+        res.status(200).json(chars);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  })
+  .post((req, res) => {
+    const char = new Character(req.body);
+    char
+      .save()
+      .then(savedChar => {
+        res.status(201).json(savedChar);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
