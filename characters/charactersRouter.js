2e9bec625f8faf4f21a19445e0c7533edd462b2b
mjd02 <mostdarryl@gmail.com>
2018-04-11 18:56:45 -0400

    populated database and almost done with character controller

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -6,4 +6,41 @@ const router = express.Router();
 
 // add endpoints here
 
+
+router
+.route('/')
+.get( (req,res)=>{
+  Character.find({})
+  .then( characters=>{
+    res.status(200).json(characters);
+  })
+  .catch( err=>{
+    res.status(500).json(err);
+  });
+})
+.post( (req,res)=>{
+  const person = new Character(req.body);
+  person.save().then(savedPerson =>{
+    res.status(200).json(savedPerson);
+  })
+  .catch(err=>{
+    res.status(500).json(err);
+  });
+})
+router
+.route('/:id')
+.get( (req,res)=>{
+  Character.findById(req.params.id,req.body)
+  .then(response=>{
+    res.status(200).json(response);
+  })
+  .catch(err=>{
+    res.status(500).json(err);
+  });
+})
+.put((req,res)=>{
+  Character.findOneAndUpdate(req.params.id,req.body)
+  .then(response=>res.status(201).json(response))
+  .catch(err=>{res.status(500).json(err);});
+});
 module.exports = router;
