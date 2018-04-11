69cf75892405279e007ea5d70fe6d021593cbe69
Juber Ahmed <a.juber@hotmail.com>
2018-04-11 16:29:59 -0400

    Working progress on charactersRouter.js

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,5 +5,30 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
+router
+  .route('/')
+  .get((req, res) => {
+    Character.find({})
+      .then(characters => {
+        res.status(200).json(characters);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  })
+
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    Character.findById(req.params.id)
+      .populate('roles', { name: 1, _id: 0 })
+      .then(user => {
+        res.status(200).json(user);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  })
 
 module.exports = router;
