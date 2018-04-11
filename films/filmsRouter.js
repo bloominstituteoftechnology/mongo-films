fc7af1980efcdad48e9967eb6a331f95c8d755f6
Carlos Andrade <carlos.andrade@outlook.com>
2018-04-11 16:35:13 -0400

    init commit

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,6 +4,29 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
-// add endpoints here
+router
+    .route('/')
+    .get((req, res)=> {
+        Film.find({})
+        .then(films => {
+            res.status(200).json(films);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+    })
+
+router
+    .route('/:id')
+    .get((req, res)=> {
+        Film.find({})
+        .where({ key: `${id}` })
+        .then(film => {
+            res.status(200).json(film);
+        })
+    })
+    // .catch(err => {
+    //     res.status(500).json(err);
+    // })    
 
 module.exports = router;
