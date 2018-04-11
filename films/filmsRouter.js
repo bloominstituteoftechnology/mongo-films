23974969b7bc63b8738c984e4ca54128d8247607
arcadia-ego <arcadia-ego@github.com>
2018-04-11 15:44:25 -0700

    get on films

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,4 +6,15 @@ const router = express.Router();
 
 // add endpoints here
 
+    router
+        .route('/')
+        .get((req, res) => {
+            Film.find({})
+            .then(films => {
+                res.status(200).json({films})
+            }).catch(err => {
+                res.status(500).json(err);
+            });
+        })
+
 module.exports = router;
