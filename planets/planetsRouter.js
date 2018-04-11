e1ca8242a4afd221410e517c081b5c25dbd2edcc
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2018-04-11 16:35:18 -0500

    WIP apis not working

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -3,7 +3,19 @@ const express = require('express');
 const Planet = require('./Planet.js');
 
 const router = express.Router();
+const Character = require('../characters/Character');
+const Specie = require('../species/Specie');
 
 // add endpoints here
+router.get('/:id', function(req, res) {
+  const { id } = req.params;
+
+  const findCharacter = Character.find({ homeworld_key: id }).select('name');
+  const findSpecies = Species.find({ homeworld_key: id }).select('name');
+
+  Promise.all([findCharacter, findSpecies]).then(data => {
+    res.json({ characters: data[0], species: data[1] });
+  });
+});
 
 module.exports = router;
--- species/speciesRouter.js
+++ species/speciesRouter.js
@@ -3,7 +3,25 @@ const express = require('express');
 const Specie = require('./Specie.js');
 
 const router = express.Router();
+const Character = require('../characters/Character');
 
 // add endpoints here
+router.put('/populate/characters', function(req, res) {
+  Species.find().then(species => {
+    const updates = species.map(doc => {
+      return Character.find()
+        .where('key')
+        .in(doc.character_keys)
+        .then(chars => {
+          doc.characters = chars.map(char => char._id);
+          return doc.save();
+        });
+    });
+
+    Promise.all(updates)
+      .then(results => res.json({ updated: results.length }))
+      .catch(err => res.status(500).json({ err }));
+  });
+});
 
 module.exports = router;
