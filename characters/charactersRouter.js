105c27fb2beea94dbbce922fdf5282cd9ef4e978
AJ Genung <DragonRydrz@me.com>
2018-04-11 16:40:11 -0400

    configured relationships to all connections

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -4,10 +4,6 @@ const Character = require('./Character.js');
 
 const router = express.Router();
 
-const serverError = error => {
-  res.status(500).json(error);
-};
-
 // add endpoints here
 
 router
