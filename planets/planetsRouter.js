eea33c1a58bd8ee49066ade3818ebf2f17c8d1b9
RiaClay <sherria.rydyn@gmail.com>
2018-04-11 18:46:56 -0500

    fixed some issues with planetRouter. Still can't get Species to populate.

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -12,34 +12,40 @@ router
 
         Planet
             .findById(req.params.id)
-            .populate('characters species')
+            .populate('species')
             .then(characterList => {
                 let key = characterList.key;
 
                 Character
                     .find({ homeworld_key: key})
                     .then(actors => {
-                        res.status(200).json(actors)
+                        characterList.characters = actors;
+                    })
+                    .then(() => {
+                        res.status(200).json(characterList)
                     })
                     .catch(err => {
                         res.status(500).json(err);
                     })
-            })
-            .then(speciesList => {
-                let key = speciesList.key
+            // })
+            // .then(speciesList => {
+            //     let key1 = speciesList.key
 
-                Specie
-                    .find({ homeworld_key: key })
-                    .then(species => {
-                        res.status(200).json(species)
-                    })
-                    .catch(err => {
-                        res.status(500).json(err)
-                    })
-
+            //     Specie
+            //         .find({ homeworld_key: key1 })
+            //         .then(formOfLife => {
+            //             speciesList.species = formOfLife;
+            //         })
+            //         .then(() => {
+            //             res.status(200).json(speciesList)
+            //         })
+            //         .catch(err => {
+            //             res.status(500).json(err);
+            //         })
             })
             .catch(err => {
                 res.status(404).json(err);
-            })
+        })
     })
+
 module.exports = router;
