35eac58d5e0dbe25ab2d966259d1e9d964ba9c09
Daniel Plott <Daniel.P.Plott@gmail.com>
2018-04-11 17:55:56 -0500

    started on characterRouter and server router, implementing final features

--- characters/Character.js
+++ characters/Character.js
@@ -14,6 +14,7 @@ const Character = mongoose.Schema({
   key: { type: Number, unique: true },
   homeworld_key: Number,
   // add homeworld field that links the character to it's planet
+  homeworld: { type: ObjectId, ref: 'Planet'}
 });
 
 module.exports = mongoose.model('Character', Character);
--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,5 +5,67 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
+router
+    .route('/')
+        .get((req, res) => {
+            Character.find({})
+            .populate('homeworld')
+            .then(ans => {
+                res.status(200).json(ans);
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
+        })
+        .post((req, res) => {
+            const newChar = new Character(req.body);
+            if (!newChar.name) res.status(400).json({error: "A name is required"})
+            else {
+                newChar
+                .save()
+                .then(res => {
+                    res.status(200).json(res);
+                })
+                .catch(err => {
+                    res.status(500).json(err);
+                })
+            }
+        })
+    
+    router
+    .route('/:id')
+    .get((req, res) => {
+        Character
+        .findById(req.params.id)
+        .populate('homeworld')
+        .then(ans => {
+            res.status(200).json(ans)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+    })
+    .put((req, res) => {
+        const updatChar = req.body
+        Character
+        .findByIdAndUpdate(req.params.id, updatChar)
+        .then(ans => {
+            res.status(200).json(updatChar)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+    })
+    .delete((req, res) => {
+        Character
+        .findByIdAndRemove(req.params.id)
+        .then(ans => {
+            res.status(200).json(ans)
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+    })
+        
 
 module.exports = router;
