b667a9589c27e171e549e8e81bbef74907d00984
joshuacoyne <josh.j.coyne@gmail.com>
2018-04-12 00:17:51 -0400

    Working on /api/characters endpoint

--- characters/Character.js
+++ characters/Character.js
@@ -16,7 +16,8 @@ const Character = mongoose.Schema({
   key: { type: Number, unique: true },
   homeworld_key: Number,
   // add homeworld field that links the character to it's planet
-  homeworld: {type: ObjectId, ref: 'Planet'}
+  homeworld: {type: ObjectId, ref: 'Planet'},
+  movies:[{type: ObjectId, ref:'Film'}],
 });
 
 module.exports = mongoose.model('Character', Character);
--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -8,14 +8,39 @@ const router = express.Router();
 router
     .route('/')
     .get((req, res) => {
-        Character.find({}).sort('key')
-        .select('_id name gender height skin_color hair_color eye_color')
-        .then(response => {
-            res.status(200).json(response);
-        })
-        .catch(error => {
-            res.status(500).json(error);
-        });
+        if(req.query.minheight) {
+            Character.find({$and:[{gender:'female'}, {height:{$gte:100}}]})
+            .then(response => {
+                res.status(200).json(response);
+            })
+            .catch(error => {
+                res.status(500).json(error);
+            });
+        } else {
+            Character.find({}).sort('key')
+            .select('_id name gender height skin_color hair_color eye_color')
+            .populate('homeworld')
+            .then(response => {
+                res.status(200).json(response);
+            })
+            .catch(error => {
+                res.status(500).json(error);
+            });
+        }
     });
 
+router
+.route('/:id')
+.get((req, res) => {
+    console.log(req.query.minheight);
+     
+    Character.find({_id: req.params.id})
+    .populate('homeworld')
+    .then(response => {
+        res.status(200).json(response);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+})
 module.exports = router;
