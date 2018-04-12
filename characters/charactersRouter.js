fa0f5da7281981d100765cb6dce3874a1fb76948
Cristina Laryea <crispad06@gmail.com>
2018-04-12 12:42:53 -0400

    added homeworld, and endpoints for characters

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -6,4 +6,28 @@ const router = express.Router();
 
 // add endpoints here
 
+router  
+    .route('/')
+    .get((req, res) => {
+        Character.find({})
+            .then(chars => {
+                res.status(200).json(chars);
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            });
+    })
+    .post((req, res) => {
+        const char = new Character(req.body);
+        
+        char
+            .save()
+            .then(savedChar => {
+                res.status(201).json(savedChar);
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            });
+    });
+
 module.exports = router;
