ce7bf8e06b9d6127671ae41c6d7a5ca7e614fdbd
Robert Rak <robertrak@hotmail.com>
2018-04-11 22:36:11 -0400

    Not so redundant after all...

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,20 +5,20 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 router
-    .route('/:id?')
+    .route('/')
     .get((req, res) => {
-      const { id } = req.params;
-        if (Object.keys(req.query).length > 0) {
-            let { producer, released } = req.query;
-            if (!released) {
-              const regex = new RegExp(producer, 'i');
 
-              Film.find({})
+      if (Object.keys(req.query).length > 0) {
+        let { producer, released } = req.query;
+
+        if (!released) {
+          const regex = new RegExp(producer, 'i');
+
+          Film.find({})
               .where({ producer: regex })
               .sort({ episode: 'ascending' })
               .populate('characters', { name: 1, gender: 1, height: 1, skin_color: 1, hair_color: 1, eye_color: 1  })
               .populate('planets', { name: 1, climate: 1, terrain: 1, gravity: 1, diameter: 1 })
-
               .then(films => {
                 res.status(200).json(films);
               })
@@ -41,7 +41,8 @@ router
                 res.status(500).json(err);
               })
             }
-        } else {
+          } else {
+
         Film.find({})
         .sort({ episode: 'ascending' })
         .populate('characters', { name: 1, gender: 1, height: 1, skin_color: 1, hair_color: 1, eye_color: 1  })
@@ -55,5 +56,20 @@ router
         })
       }
     });
-    
+
+router
+    .route('/:id')
+    .get((req, res) => {
+      const id = req.params.id;
+
+        Film.find({})
+        .where({ key: `${id}` })
+        .then(film => {
+            res.status(200).json(film);
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        })
+    })
+
 module.exports = router;
