627f8bad638ee885c3a6863225549447d4be3b81
Cesar Mejia Leiva <cmejia@gmail.com>
2018-04-12 15:59:31 -0700

    all endpoints complete, except for put

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -20,11 +20,23 @@ router.route('/').get((req, res) => {
 router.route('/:id').get((req, res) => {
   const { id } = req.params;
 
-  let query1 = Character.find({ homeworld: id }).select({ name: 1, _id: 0 });
-  let query2 = Specie.find({ homeworld: id }).select({ name: 1, _id: 0 });
-  query1.then(chars => {
-    res.status(200).json(chars);
+  const findCharacters = Character.find({ homeworld: id }).select({
+    name: 1,
+    _id: 0,
   });
+
+  const findSpecies = Specie.find({ homeworld: id }).select({
+    name: 1,
+    _id: 0,
+  });
+
+  Promise.all([findCharacters, findSpecies])
+    .then(results => {
+      res.status(200).json({ characters: results[0], species: results[1] });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
