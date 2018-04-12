9061f0a32776512d34719aa300249c66f050cd9c
Luis Diaz <luisd824@gmail.com>
2018-04-12 16:26:30 -0400

    filtered producers and release date in filmsRouter and started on characters

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -4,6 +4,23 @@ const Character = require('./Character.js');
 
 const router = express.Router();
 
-// add endpoints here
-
+// GET all characters
+router.get('/', (req, res) => {
+  Character.find({})
+    .then(characters => {
+      res.status(200).json(characters);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
+// GET character by id
+// missing movies property to add to charcters
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  let idQuery = Character.findById(id);
+  idQuery.populate('homeworld').then(characters => {
+    res.status(200).json(characters);
+  });
+});
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,8 +6,11 @@ const router = express.Router();
 
 // GET
 router.get('/', (req, res) => {
-  Film.find({})
-    .sort('episode')
+  const { producer, release_date } = req.query;
+  const producerRegx = new RegExp(producer, 'i');
+  const releaseRegx = new RegExp(release_date, 'i');
+  const query = Film.find({}).sort('episode');
+  query
     .populate('characters planets', {
       _id: 1,
       name: 1,
@@ -16,12 +19,13 @@ router.get('/', (req, res) => {
       skin_color: 1,
       hair_color: 1,
       eye_color: 1,
-      name: 1,
       climate: 1,
       terrain: 1,
       gravity: 1,
       diameter: 1
     })
+    .where({ producer: producerRegx })
+    .where({ release_date: releaseRegx })
     .then(films => {
       res.status(200).json(films);
     })
@@ -29,32 +33,28 @@ router.get('/', (req, res) => {
       res.status(500).json(error);
     });
 });
-
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  Film.findById(id)
-
-    .then(film => {
-      if (!film) {
-        res.status(404).json({ message: 'that film doesnt exist' });
-      } else {
-        res.status(200).json(film);
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
-//POST
-router.post('/', (req, res) => {
-  const film = new Film(req.body);
-  film
-    .save()
-    .then(movie => {
-      res.status(201).json(movie);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
+// ******EXTRA*******
+// router.get('/:id', (req, res) => {
+//   const { id } = req.params;
+//   Film.findById(id)
+//
+//     .then(film => {
+//       res.status(200).json(film);
+//     })
+//     .catch(error => {
+//       res.status(500).json(error);
+//     });
+// });
+// //POST
+// router.post('/', (req, res) => {
+//   const film = new Film(req.body);
+//   film
+//     .save()
+//     .then(movie => {
+//       res.status(201).json(movie);
+//     })
+//     .catch(error => {
+//       res.status(500).json(error);
+//     });
+// });
 module.exports = router;
