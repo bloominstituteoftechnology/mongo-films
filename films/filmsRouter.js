8d47e746f81833140866b59056278a0ffe6ea89a
Cristina Laryea <crispad06@gmail.com>
2018-04-12 13:05:45 -0400

    added fields in film, and endpoints, displaying and able to post

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,4 +6,44 @@ const router = express.Router();
 
 // add endpoints here
 
+router
+    .route('/')
+    .get((req, res) => {
+        Film.find({})
+            .sort({ key: 1 })
+            .populate('characters', {
+                _id: 1,
+                name: 1,
+                gender: 1,
+                height: 1,
+                skin_color: 1,
+                hair_color: 1,
+                eye_color: 1,
+            })
+            .populate('planets', {
+                name: 1,
+                climate: 1,
+                terrain: 1,
+                gravity: 1,
+                diameter: 1, 
+            })
+            .then(films => {
+                res.status(200).json(films);
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            });
+    })
+        .post((req, res) => {
+            const film = new Film(req.body);
+            film
+                .save()
+                .then(savedFilm => {
+                    res.status(201).json(savedFilm);
+                })
+                .catch(err => {
+                    res.status(500).json(err);
+                });
+        });
+
 module.exports = router;
