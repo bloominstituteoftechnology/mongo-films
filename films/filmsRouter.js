2af291b3c174bf834ecc29ff4df81620954208c9
mjd02 <mostdarryl@gmail.com>
2018-04-12 17:22:58 -0400

    finished film population

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,4 +6,27 @@ const router = express.Router();
 
 // add endpoints here
 
+router
+.route('/')
+.get( (req,res)=>{
+  Film.find({}).sort('episode')
+  .populate('characters')
+  .populate('planets')
+  .exec(function(err,films){
+    if(err){
+      handleError(err);
+    }
+    films = films.map(e=>{
+      e.characters = e.characters.map(f=>{
+        return {_id:f._id,name:f.name,gender:f.gender,height:f.height,skin_color:f.skin_color,eye_color:f.eye_color};
+      });
+      e.planets = e.planets.map(f=>{
+        return {_id:f._id,name:f.name,climate:f.climate,terrain:f.terrain,gravity:f.gravity,diameter:f.diameter};
+      });
+      return e;
+    });
+    res.status(200).json(films);
+  })
+});
+
 module.exports = router;
