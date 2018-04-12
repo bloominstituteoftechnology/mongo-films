901c9cde60c4987018d4af76ea57d3ca71252e39
joshuacoyne <josh.j.coyne@gmail.com>
2018-04-11 23:34:51 -0400

    film endpoint really complete

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -13,6 +13,8 @@ router
             Film
             .find({producer: {$regex:`${req.query.producer}`, $options:'i'}})
             .select('title producer')
+            .populate('characters', '_id name gender height skin_color hair_color eye_color')
+            .populate('planets', {name:1, climate:1, terrain:1, gravity:1, diameter:1, _id:0})
             .then(response => {
                 res.status(200).json(response);
             })
@@ -23,6 +25,8 @@ router
             console.log(req.query.released);
             Film
             .find({release_date: {$regex: `^${req.query.released}`}})
+            .populate('characters', '_id name gender height skin_color hair_color eye_color')
+            .populate('planets', {name:1, climate:1, terrain:1, gravity:1, diameter:1, _id:0})
             .then(response => {
                 res.status(200).json(response);
             })
