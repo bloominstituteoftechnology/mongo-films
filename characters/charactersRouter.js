8f476eb377ee0e229589279082657623cc63a69d
Carlos Andrade <carlos.andrade@outlook.com>
2018-04-12 15:34:07 -0400

    progress

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -20,11 +20,17 @@ router
 router
     .route('/:id')
     .get((req, res) => {
-      const id = req.params.id;
+        let name = req.params.id;
+     
+
+        const regex = new RegExp(name, 'i');
+        
+
         Character.find({})
         .populate('homeworld')
-        .then(characters => {
-            res.status(200).json(characters[`${id}`]);
+        .where({name: regex})
+        .then(character => {
+            res.status(200).json(character);
         })
         .catch(err => {
           res.status(500).json(err);
