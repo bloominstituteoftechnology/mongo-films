148dbf1d15b042a5325cfb81e08e9d6378fe2191
arcadia-ego <arcadia-ego@github.com>
2018-04-13 08:30:07 -0700

    vehicle hell

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const Character = require("./Character.js");
 
+const Vehicle = require("../vehicles/Vehicle.js");
+
 const Film = require("../films/Film.js");
 
 const router = express.Router();
@@ -19,13 +21,29 @@ router.route("/:id").get(async (req, res) => {
       mongoose.Types.ObjectId(req.params.id)
     );
     const moviesMapped = movies.map(title => title.title);
-    res.json({...charByID._doc, movies: moviesMapped});
+    res.json({ ...charByID._doc, movies: moviesMapped });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error });
   }
 });
 
-// router.route("/:id/vehicles")
+router.route("/:id/vehicles").get(async (req, res) => {
+  try {
+    const charVehicles = await Vehicle.find({})
+    checkedVehicles = 0;
+    const mappedVehicles = charVehicles.map(vehicle => {
+      if(vehicle.includes(req.params.id)){
+       res.json({vehicle})
+      }
+    });
+    // const checkedVehicles = mappedVehicles.includes(req.params.id);
+    console.log(mappedVehicles)
+    // console.log(mappedVehicles[20] == (req.params.id));
+    // res.status(200).json({ checkedVehicles });
+  } catch (error) {
+    res.status(500).json({ error: "server had an error" });
+  }
+});
 
 module.exports = router;
