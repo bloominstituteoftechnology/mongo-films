09be6b9c41dc27ad7538906b87a11ba20711dd2f
zackhitch <zackhitch@users.noreply.github.com>
2018-04-12 22:31:33 -0700

    Attempt at character gte 100 female - ZH

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -7,25 +7,28 @@ const Vehicle = require('../vehicles/Vehicle');
 const router = express.Router();
 
 // add endpoints here
+// Find all female characters taller than 100cm (/api/characters?minheight=100)
 router.route('/').get((req, res) => {
   const { minheight } = req.query;
-  let pattern;
-  let obj = {};
   if (minheight) {
-    pattern = new RegExp(minheight);
-    obj = { height: pattern };
+    Character.find()
+      .where({ height: { $gte: minheight }, gender: 'female' })
+      .then(resposne => {
+        res.status(200).json(response);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  } else {
+    Character.find()
+      .then(response => {
+        res.status(200).json(response);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
   }
-  Character.find(obj)
-    // .where({ gender: 'female' })
-    .then(response => {
-      res.status(200).json(response);
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
 });
-
-// Find all female characters taller than 100cm (/api/characters?minheight=100)
 
 router.route('/:id/vehicles').get((req, res) => {
   const { id } = req.params;
