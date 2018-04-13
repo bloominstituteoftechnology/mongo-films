aefe8279131f2338dd102b732d4149ce43eb3d15
KJ Sourcing <tmkogwork@gmail.com>
2018-04-12 17:49:10 -0700

    planets route complete

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,9 +1,34 @@
-const express = require('express');
+const express = require("express");
 
-const Planet = require('./Planet.js');
+const Planet = require("./Planet.js");
+const Character = require("../characters/Character.js");
+const Specie = require("../species/Specie.js");
 
 const router = express.Router();
 
-// add endpoints here
+
+router.route("/").get((req, res) => {
+
+	Planet.find()
+		.then(planets => {
+			res.status(200).json(planets);
+		})
+		.catch(err => {
+			res.status(500).json(err);
+		});
+});
+
+router.route("/:id").get((req, res) => {
+	const { id } = req.params;
+
+	Character.find({ homeworld: id })
+		.then(chars => {
+    Specie.find({ homeworld: id })
+    	.then(species => {
+     	 res.status(200).json({ Character: chars, Specie: species });
+    });
+  });
+});
+
 
 module.exports = router;
