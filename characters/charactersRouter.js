4e3f69094ce2e7cb8e74937f70fec67d5e8eea3f
Justin Kaseman <justinkaseman@live.com>
2018-04-12 17:48:45 -0700

    Finish characters end points

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const Character = require('./Character.js');
+const Film = require('../films/Film.js');
+const Vehicle = require('../vehicles/Vehicle.js');
 
 const router = express.Router();
 
@@ -24,15 +26,14 @@ router.get('/:id', (req, res) => {
 	const id = req.params.id;
 	//id check
 	Character.findById(id)
-		.populate({
-			path: 'movies',
-			select: {},
-			model: 'Film',
-			match: { characters: id },
-		})
 		.populate('homeworld')
 		.then(char => {
-			res.status(200).json(char);
+			Film.find({ characters: id })
+				.select('title producer director episode release_date')
+				.then(films => {
+					const character = { ...char._doc, movies: films };
+					res.status(200).json(character);
+				});
 		})
 		.catch(err => {
 			res.status(500).json('not found');
@@ -40,17 +41,10 @@ router.get('/:id', (req, res) => {
 });
 
 router.get('/:id/vehicles', (req, res) => {
-	if (req.query.producer) {
-		const regex = new RegExp(req.query.producer, 'i');
-		Character.find({})
-			.where({ producer: regex })
-			.then(vehicles => {
-				res.status(200).json(vehicles);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	}
+	Vehicle.find({ pilots: req.params.id })
+		.select('vehicle_class')
+		.populate('pilots', 'name')
+		.then(vehicles => res.send(vehicles));
 });
 
 module.exports = router;
--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,9 +1,20 @@
 const express = require('express');
 
 const Planet = require('./Planet.js');
+const Character = require('../characters/Character.js');
+const Specie = require('../species/Specie.js');
 
 const router = express.Router();
 
-// add endpoints here
+router.get('/:id', (req, res) => {
+	Character.find({ homeworld_key: req.params.id })
+		.then(chars => {
+			Specie.find({ homeworld_key: req.params.id }).then(species => {
+				const response = { ...chars, ...species };
+				res.status(200).json(response);
+			});
+		})
+		.catch(error => console.log(error));
+});
 
 module.exports = router;
