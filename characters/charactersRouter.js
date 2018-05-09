b4a93f2b5b5f87867d35a658f93e982f55fed6af
Velsu <riftmage50@gmail.com>
2018-05-09 23:51:25 +0200

    Completed Characters endpoints

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -7,14 +7,40 @@ const router = express.Router();
 // add endpoints here
 
 router.route("/:id").get((req, res) => {
-  Character.find()
-    .populate("characters", "name")
+  Character.findById(req.params.id)
+    .populate("homeworld")
     .then(response => {
       res.status(200).json(response);
     })
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+// db.users.find({$or: [{name: {$regex: "^Da"}}, {name: {$regex: "^Ali"}}]})
+
+router.route("/").get((req, res) => {
+  if (req.query.minheight) {
+    Character.find({
+      $and: [{ gender: "female" }, { height: { $gte: req.query.minheight } }]
+    })
+      .populate("homeworld")
+      .then(response => {
+        res.status(200).json(response);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    Character.find()
+      .populate("homeworld")
+      .then(response => {
+        res.status(200).json(response);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  }
 });
 
 module.exports = router;
