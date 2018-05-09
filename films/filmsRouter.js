ea5e87011999774d61d4289645715df96ed85b9b
unknown <sycode@gmail.com>
2018-05-09 14:16:41 -0600

    films showing by ascending order

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -3,12 +3,11 @@ const express = require("express");
 const Film = require("./Film.js");
 
 const router = express.Router();
-// let query =
-router.route("/").get((req, res) => {
-  Film.find()
-    .sort({
-      episode: "asc"
-    })
+// let query = Film.find();
+router
+  .route("/")
+  .get((req, res) => {
+    Film.find().sort({episode: "asc"})
     .then(films => {
       console.log(films);
       res.status(200).json(films);
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const planetsRouter = require('./planets/planetsRouter.js');
 const server = express();
 
 db
-  .connectTo('starwars')
+  .connectTo('MongoFilms')
   .then(() => console.log('\n... API Connected to Database ...\n'))
   .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
 
