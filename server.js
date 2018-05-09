f84fd1ceee6275a734e421801cfdfe3b91cb47aa
axhon <axhonruiz@gmail.com>
2018-05-09 09:55:16 -1000

    can now use cloud solution

--- server.js
+++ server.js
@@ -1,39 +1,43 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require('express')
+const helmet = require('helmet')
 
-const db = require('./data/db.js');
-const charactersRouter = require('./characters/charactersRouter.js');
-const filmsRouter = require('./films/filmsRouter.js');
-const speciesRouter = require('./species/speciesRouter.js');
-const starshipsRouter = require('./starships/starshipsRouter.js');
-const vehiclesRouter = require('./vehicles/vehiclesRouter.js');
-const planetsRouter = require('./planets/planetsRouter.js');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-const server = express();
+const db = require('./data/db.js')
+const charactersRouter = require('./characters/charactersRouter.js')
+const filmsRouter = require('./films/filmsRouter.js')
+const speciesRouter = require('./species/speciesRouter.js')
+const starshipsRouter = require('./starships/starshipsRouter.js')
+const vehiclesRouter = require('./vehicles/vehiclesRouter.js')
+const planetsRouter = require('./planets/planetsRouter.js')
+
+const server = express()
 
 db
-  .connectTo('starwars')
+  .connectTo('starwars', process.env.MONGO_URI)
   .then(() => console.log('\n... API Connected to Database ...\n'))
-  .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
+  .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err))
 
-server.use(helmet());
-server.use(express.json());
+server.use(helmet())
+server.use(express.json())
 
-server.use('/api/characters', charactersRouter);
-server.use('/api/films', filmsRouter);
-server.use('/api/species', speciesRouter);
-server.use('/api/starships', starshipsRouter);
-server.use('/api/vehicles', vehiclesRouter);
-server.use('/api/planets', planetsRouter);
+server.use('/api/characters', charactersRouter)
+server.use('/api/films', filmsRouter)
+server.use('/api/species', speciesRouter)
+server.use('/api/starships', starshipsRouter)
+server.use('/api/vehicles', vehiclesRouter)
+server.use('/api/planets', planetsRouter)
 
-server.get('/', (req, res) => res.send('API Running...'));
+server.get('/', (req, res) => res.send('API Running...'))
 
 server.use((error, req, res, next) => {
   console.log(error)
   res.send({ error })
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 server.listen(port, () =>
   console.log(`\n\nAPI running on http://localhost:${port}`)
-);
+)
