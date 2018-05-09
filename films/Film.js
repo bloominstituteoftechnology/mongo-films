5fd959d0d3f11e3f93548f4a8c797b1f04ac736a
Jared Cuffe <jcuffe@gmail.com>
2018-05-09 12:35:16 -0700

    Model refs

--- films/Film.js
+++ films/Film.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const Film = new mongoose.Schema({
@@ -16,6 +17,10 @@ const Film = new mongoose.Schema({
   key: { type: Number, unique: true },
   starship_ids: [Number],
   vehicle_ids: [Number],
+  starships: [{ type: ObjectId, ref: 'Starship' }],
+  vehicles: [{ type: ObjectId, ref: 'Vehicle' }],
+  characters: [{ type: ObjectId, ref: 'Character' }],
+  species: [{ type: ObjectId, ref: 'Specie' }]
   // add fields for starships, vehicles, planets, characters and species
   // to link them to the corresponding model
 });
