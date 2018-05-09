e4274af227b5ea25e7b88955e573fca5e09c6ed5
Yasin Shuman <email@yasin.io>
2018-05-09 14:59:05 -0700

    fixed get function on character controller

--- characters/Character.js
+++ characters/Character.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const Character = mongoose.Schema({
-	name: { type: String, required: true },
-	edited: Date,
-	created: Date,
-	gender: String,
-	height: String,
-	hair_color: String,
-	skin_color: String,
-	eye_color: String,
-	birth_year: String,
-	key: { type: Number, unique: true },
-	homeworld_key: Number,
-	homeworld: {
-		type: ObjectId,
-		ref: 'Planet',
-	},
-	// add homeworld field that links the character to it's planet
+  name: { type: String, required: true },
+  edited: Date,
+  created: Date,
+  gender: String,
+  height: Number,
+  hair_color: String,
+  skin_color: String,
+  eye_color: String,
+  birth_year: String,
+  key: { type: Number, unique: true },
+  homeworld_key: Number,
+  homeworld: {
+    type: ObjectId,
+    ref: "Planet"
+  }
+  // add homeworld field that links the character to it's planet
 });
 
-module.exports = mongoose.model('Character', Character);
+module.exports = mongoose.model("Character", Character);
--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-const Character = require('./Character.js');
+const Character = require("./Character.js");
 
 const router = express.Router();
 
-// router.route('/').get(get);
-// // .post(post);
-
-//
-router.route('/').get(get);
+router.route("/").get(get);
 
 function get(req, res) {
-	res.send('character running');
+  Character.find()
+    .then(characters => {
+      res.status(200).json(characters);
+    })
+    .catch(err => {
+      errorMessage: "The friends information could not be retrieved.";
+    });
 }
 
 module.exports = router;
