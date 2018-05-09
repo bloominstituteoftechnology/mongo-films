638cb32bb80ae87e528870679c4081543cc2ca94
Imran Ahmed <jappleleaf@gmail.com>
2018-05-09 18:37:49 -0400

    WIP

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,12 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
+    let query  = Film.find();
+    query.sort({episode: 1})
+    // Film.find()
+    .then(film => res.status(200).json(film))
+    .catch(err => res.status(500).json(err))
+})
 
 module.exports = router;
