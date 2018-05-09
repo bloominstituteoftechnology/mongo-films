b5a64a3c11dab5ea5621569c1b73a82d29383faa
Jared Cuffe <jcuffe@gmail.com>
2018-05-09 13:25:44 -0700

    Film endpoint refactored to match spec

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,8 +6,16 @@ const router = express.Router();
 
 // add endpoints here
 router.use('/', (req, res, next) => {
-  Film.find({ $text: { $search: req.query.producer || req.query.released } })
-    .populate(['starships', 'vehicles', 'characters', 'species', 'planets'])
+  const { producer, released } = req.query
+  const query = (producer || released)
+    ? Film.find({ $text: { $search: producer || released } })
+    : Film.find()  
+  
+  query
+    .sort('episode')  
+    .populate('characters', '_id name gender height skin_color hair_color eye_color')
+    .populate('planets', 'name climate terrain gravity diameter')
+    .select('producer release_date')
     .then(films => res.send(films))
     .catch((err) => next(err))
 })
