919dee08b6dda8be30a481c587d3aa00944d96ff
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-09 18:35:53 -0400

    Endpoint for films produced by Gary Kurtz

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,30 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
 
+    let query = Film.find();
+    
+    query.sort({ episode: 1 })
+
+    query
+    .then(films => {
+        res.json(films)
+    })
+    .catch(err => {
+        res.json({ Error: err})
+    })
+})
+
+router.get('/?producer=gary+kurtz', (req, res) => {
+
+    Film
+    .find({ producer: { regex: /Gary Kurtz/ }})
+    .then(gary => {
+        res.json(gary)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
 module.exports = router;
