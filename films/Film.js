43a10f0bd10cc34d14d8f76956cbff47994a4ef5
Justin Hong <jhong2009@gmail.com>
2018-05-09 13:18:59 -0700

    in planetRouter.js: fixed GET request

--- films/Film.js
+++ films/Film.js
@@ -1,48 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const options = {
   timestamp: true,
-  strict: false,
+  strict: false
 };
 
-const Film = new mongoose.Schema({
-  created: { type: Date, default: Date.now },
-  episode: Number,
-  edited: { type: Date, default: Date.now },
-  planet_ids: [Number],
-  producer: String,
-  title: { type: String, required: true },
-  director: String,
-  release_date: String,
-  opening_crawl: String,
-  character_ids: [Number],
-  specie_ids: [Number],
-  key: { type: Number, unique: true },
-  starship_ids: [Number],
-  vehicle_ids: [Number],
-  // add fields for starships, vehicles, planets, characters and species
-  // to link them to the corresponding model
-  starships: {
-    type: ObjectId,
-    ref: 'Starship',
+const Film = new mongoose.Schema(
+  {
+    created: { type: Date, default: Date.now },
+    episode: Number,
+    edited: { type: Date, default: Date.now },
+    planet_ids: [Number],
+    producer: String,
+    title: { type: String, required: true },
+    director: String,
+    release_date: String,
+    opening_crawl: String,
+    character_ids: [Number],
+    specie_ids: [Number],
+    key: { type: Number, unique: true },
+    starship_ids: [Number],
+    vehicle_ids: [Number],
+    // add fields for starships, vehicles, planets, characters and species
+    // to link them to the corresponding model
+    starships: {
+      type: ObjectId,
+      ref: "Starship"
+    },
+    vehicles: {
+      type: ObjectId,
+      ref: "Vehicle"
+    },
+    planets: {
+      type: ObjectId,
+      ref: "Planet"
+    },
+    characters: {
+      type: ObjectId,
+      ref: "Character"
+    },
+    species: {
+      type: ObjectId,
+      ref: "Specie"
+    }
   },
-  vehicles: {
-    type: ObjectId,
-    ref: 'Vehicle',
-  },
-  planets: {
-    type: ObjectId,
-    ref: 'Planet',
-  },
-  characters: {
-    type: ObjectId,
-    ref: 'Character',
-  },
-  species: {
-    type: ObjectId,
-    ref: 'Specie',
-  },
-}, options);
+  options
+);
 
-module.exports = mongoose.model('Film', Film);
+module.exports = mongoose.model("Film", Film);
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require("express");
 
-const Film = require('./Film.js');
+const Film = require("./Film.js");
 
 const router = express.Router();
 
 // add endpoints here
-router.route('/')
-  .get((req, res) => {
-    Film.find()
-      .then(films => {
-        res.status(200).json(films);
-      })
-      .catch(err => {
-        res.status(500).json({
-          err: "Films cannot be retrieved"
-        })
-      })
-  })
+router.route("/").get((req, res) => {
+	Film.find()
+		.then(films => {
+			res.status(200).json(films);
+		})
+		.catch(err => {
+			res.status(500).json({
+				err: "Films cannot be retrieved"
+			});
+		});
+});
 
 module.exports = router;
