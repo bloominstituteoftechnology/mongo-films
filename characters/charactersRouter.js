f547dffea97145828ee7731eff7078f6f0c0ae0a
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-09 17:42:58 -0400

    populate character with homeworld

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Character = require('./Character.js');
+const Planet = require('../planets/Planet');
 const router = express.Router();
 
 router.route('/')
@@ -12,7 +13,11 @@ router.route('/')
 router.route('/:id')
   .get((req, res) => {
     const { id } = req.params;
-    Character.findById(id)
+    const query = Character.findById(id);
+
+    query.populate('homeworld', 'name');
+    
+    query
       .then(char => res.json(char))
       .catch(err => res.status(500).json("Cannot fetch character with the provided ID."))
   })
