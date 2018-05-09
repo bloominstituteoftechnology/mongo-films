cebc7ee7e3519841056e266f413d3b63b2535e88
nickbrenn <nbren@tutanota.com>
2018-05-09 17:38:53 -0400

    Ordered films by episode (starting with 1)

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -7,6 +7,30 @@ const router = express.Router();
 router.get("/", (req, res) => {
   Film.find()
     .then(films => {
+      films = films.sort(function(a, b) {
+        let episodeA = a.episode;
+        let episodeB = b.episode;
+        if (episodeA < episodeB) {
+          return -1;
+        }
+        if (episodeA > episodeB) {
+          return 1;
+        }
+        return 0;
+        // console.log(a);
+        // return b - a;
+      });
+      // sortedNotes.sort(function(a, b) {
+      //   let titleA = a.title.toLowerCase();
+      //   let titleB = b.title.toLowerCase();
+      //   if (titleA < titleB) {
+      //     return -1;
+      //   }
+      //   if (titleA > titleB) {
+      //     return 1;
+      //   }
+      //   return 0;
+      // })
       res.status(200).json(films);
     })
     .catch(err => {
