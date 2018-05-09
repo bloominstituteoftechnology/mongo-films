f04f0b9074b1aa1a307fd175798742b33f58c1a1
Devon Bacon <devon.bacon@gmail.com>
2018-05-09 15:58:42 -0700

    filmRouter.js - progress in search by producer functionality

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,9 +6,19 @@ const router = express.Router();
 
 // add endpoints here
 router.route("/").get((req, res) => {
-	Film.find()
-		.then(films => {
-			res.status(200).json(films);
+  const { producer } = req.query;
+  let query = Film.find();
+
+  Film.find().sort('episode')
+    .then(films => {
+      if(producer === 'gary kurtz') {
+        query.where({ producer: /Gary Kurtz/i })
+          .then(films => {
+            res.status(200).json(films);
+          })
+      } else {
+        res.status(200).json(films);
+      }
 		})
 		.catch(err => {
 			res.status(500).json({
@@ -16,5 +26,7 @@ router.route("/").get((req, res) => {
 			});
 		});
 });
+
+
 
 module.exports = router;
