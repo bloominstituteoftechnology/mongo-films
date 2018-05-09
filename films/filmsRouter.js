a815b1d95e96a442f5d0d0d1a6deb95dbd534585
Henry K <mykim1891@hotmail.com>
2018-05-09 15:56:04 -0700

    populate(characters) working; working on planets

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,7 +6,13 @@ const router = express.Router();
 
 // add endpoints here
 router.get("/", (req, res) => {
-  Film.find().sort('episode')
+  Film.find()
+    .sort("episode")
+    .populate(
+      "characters",
+      "_id name gender height skin_color hair_color eye_color"
+    )
+    .populate("planets", "name, climate, terrain, gravity, diameter")
     .then(film => {
       res.status(201).json(film);
     })
