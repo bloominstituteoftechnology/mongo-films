e587ed0cf35a490b770a382950582ab3213c38d9
unknown <sycode@gmail.com>
2018-05-09 14:51:39 -0600

    find and return character

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -4,6 +4,17 @@ const Character = require('./Character.js');
 
 const router = express.Router();
 
-// add endpoints here
+router
+    .route("/:id")
+    .get((req, res) => {
+      const id = req.params.id
+      Character.findById(id)
+      .then(character => {
+            res.status(200).json(character);
+      })
+      .catch(error => {
+        res.status(500).json("Error finding that character");
+      })
+    })
 
 module.exports = router;
