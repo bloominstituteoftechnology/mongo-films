bd7689b5869b9eb101f674b036fc29b219231d5e
2940cristian <2940cristian@gmail.com>
2018-05-09 12:34:42 -0700

    Imported data and did router handlers for characters/films

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -3,7 +3,76 @@ const express = require('express');
 const Character = require('./Character.js');
 
 const router = express.Router();
+  
+  router.get("/", (req, res) => {
+    Character.find().then(characters => {
+      res.status(200).json(characters)
+    }).catch(err => {
+      res.status(500).json({
+        errorMessage: "The character information could not be retrieved."})
+    })
+  })
+  
+  router.get("/:id", (req, res) => {
+    const id = req.params.id
+    Character.findById(id).then(character => {
+      res.status(200).json(character)
+    }).catch(err => {
+      res.status(404).json({
+        message: "A character with that id could not be found"
+      })
+    })
+  })
 
-// add endpoints here
+  
+  router.post("/", (req, res) => {
+    const characterData = req.body;
+    const character = new Character(characterData);
+      
+    character.save().then(character => {
+        res.status(200).json({
+          message: "Successfuly saved new character to database"
+        })
+      }).catch(err => {
+        res.status(500).json({
+          errorMessage: "There was an error while saving the character to the database."
+        })
+      })
+  })
+  
+  
+  router.delete("/:id", (req, res) => {
+    const id = req.params.id
+
+    Character.findByIdAndRemove(id).then(character => {
+        res.status(200).json({
+            message: "Character has been deleted from the database"
+        })
+    }).catch(err => {
+      res.status(500).json({
+        errorMessage: "The character could not be removed"
+        })
+    })
+})
+  
+  
+  router.put("/:id", (req, res)=> {
+    const id = req.params.id;
+    const input = req.body;
+  
+    Character.findByIdAndUpdate(id, input).then(character => {
+        res.status(200).json({
+            message: "Character has been succesfully updated"
+          })
+        }).catch(err => {
+            res.status(500).json({
+      errorMessage: "The character information could not be modified."
+    })
+  })
+});
+
+
+
+
 
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,6 +4,71 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
-// add endpoints here
+router.get("/", (req, res) => {
+    Film.find().then(films => {
+      res.status(200).json(films)
+    }).catch(err => {
+      res.status(500).json({
+        errorMessage: "The film information could not be retrieved."})
+    })
+  })
+  
+  router.get("/:id", (req, res) => {
+    const id = req.params.id
+    Film.findById(id).then(film => {
+      res.status(200).json(film)
+    }).catch(err => {
+      res.status(404).json({
+        message: "A film with that id could not be found"
+      })
+    })
+  })
+
+  
+  router.post("/", (req, res) => {
+    const filmData = req.body;
+    const film = new Film(filmData);
+      
+    film.save().then(film => {
+        res.status(200).json({
+          message: "Successfuly saved new film to database"
+        })
+      }).catch(err => {
+        res.status(500).json({
+          errorMessage: "There was an error while saving the film to the database."
+        })
+      })
+  })
+  
+  
+  router.delete("/:id", (req, res) => {
+    const id = req.params.id
+
+    Film.findByIdAndRemove(id).then(film => {
+        res.status(200).json({
+            message: "film has been deleted from the database"
+        })
+    }).catch(err => {
+      res.status(500).json({
+        errorMessage: "The film could not be removed"
+        })
+    })
+})
+  
+  
+  router.put("/:id", (req, res)=> {
+    const id = req.params.id;
+    const input = req.body;
+  
+    Film.findByIdAndUpdate(id, input).then(film => {
+        res.status(200).json({
+            message: "Film has been succesfully updated"
+          })
+        }).catch(err => {
+            res.status(500).json({
+      errorMessage: "The film information could not be modified."
+    })
+  })
+});
 
 module.exports = router;
