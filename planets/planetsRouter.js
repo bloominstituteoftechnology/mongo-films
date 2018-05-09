0b84dfacd2f060e6f2ff7dbe6bbd0143e706eacf
Natalie Schennum <natalieschennum@gmail.com>
2018-05-09 15:46:00 -0700

    Planet Router for Characters and Species

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const Planet = require('./Planet.js');
+const Planet = require("./Planet.js");
+const Character = require("../characters/Character.js");
+const Specie = require("../species/Specie.js");
 
 const router = express.Router();
 
 // add endpoints here
+router.get("/:id", async (req, res) => {
+  const chars = await Character.find({ homeworld: req.params.id });
+  const species = await Specie.find({ homeworld: req.params.id });
+
+  res.status(200).json({ chars, species });
+});
 
 module.exports = router;
