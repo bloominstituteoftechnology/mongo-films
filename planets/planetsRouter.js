19567c54fc46e596e4cac270560df77dc7f79832
cgoez <goez2001@gmail.com>
2018-05-09 14:28:09 -0700

    planetsRouter GET /

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require("express");
 
-const Planet = require('./Planet.js');
+const Planet = require("./Planet.js");
 
 const router = express.Router();
 
-// add endpoints here
+// /api/planets
+
+// GET / ; get all planets
+router.route("/").get((req, res) => {
+  Planet.find()
+    .then(planets => {
+      res.status(200).json(planets);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 module.exports = router;
