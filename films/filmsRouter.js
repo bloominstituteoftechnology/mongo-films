8d9b7577c4f1f1ca4c186c063241a7654d6478ab
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-09 14:50:01 -0700

    /api/films displayed to Postman

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,7 +5,7 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
-router.get('/', (req, res => {
+router.get('/', (req, res) => {
     const producerFilter = req.query.producer;
     const filmReleased = req.query.released;
 
@@ -13,7 +13,9 @@ router.get('/', (req, res => {
         .populate('characters', 'name gender height skin_color hair_color eye_color')
         .populate('planets', 'name climate terrain gravity diameter');
 
-
+    query.then(films => {
+        res.json(films);
+    })
 });
 
 module.exports = router;
