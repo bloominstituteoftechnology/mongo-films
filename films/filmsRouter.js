be90f9241c7b0438a5aa226320d32ab0f41e1f31
unknown <sycode@gmail.com>
2018-05-09 14:43:33 -0600

    films by producer & release date

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -8,24 +8,23 @@ router.get("/", (req, res) => {
   const producerFilter = req.query.producer;
   const releaseFilter = req.query.released;
 
-  let query = Film.find()
-      .sort({episode: 'asc'});
+  let query = Film.find().sort({ episode: "asc" });
 
   if (producerFilter) {
-      const filter = new RegExp(producerFilter, "i");
-      query.where({ producer: filter });
+    const filter = new RegExp(producerFilter, "i");
+    query.where({ producer: filter });
   }
   if (releaseFilter) {
-      const filter = new RegExp(releaseFilter, "i");
-      query.where({ release_date: filter });
+    const filter = new RegExp(releaseFilter, "i");
+    query.where({ release_date: filter });
   }
   query
-      .then(films => {
-          res.send(films);
-      })
-      .catch(err => {
-          res.status(400).send({ error: err });
-      });
+    .then(films => {
+      res.send(films);
+    })
+    .catch(err => {
+      res.status(400).send({ error: err });
+    });
 });
 
 module.exports = router;
