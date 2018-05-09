04c09c825ab2ed721287195d248d755d1331a3ec
totsuya <supertatortots@gmail.com>
2018-05-09 17:07:35 -0400

    added some endpoitns for Film Router

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,28 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
+  Film.find().sort('episode')
+  .then( film => {
+    res.status(200).json(film);
+  })
+  .catch( error => {
+    res.status(500).json ({ error: "Films could not be retrieved, try again later" })
+  })
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params;
+  Film.findById(id)
+  .then (film => {
+    res.status(201).json(film);
+  })
+  .catch (error => {
+    res.status(500).json ({ error: "Film ID could not be retrieved." })
+  })
+});
+
+
+
 
 module.exports = router;
