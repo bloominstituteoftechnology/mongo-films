ad85ebdcd3ed5fef34326c89897810d59df94092
ajf412 <aaronjfaulkner@yahoo.com>
2018-05-09 18:54:53 -0400

    Setup complete.  Can pull movies and individual characters.  Regex is bogging everything down.

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,5 +5,48 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
+router
+  .route('/')
+  .get((req, res) => {
+    Character.find()
+      .then(characters => {
+        res.status(200).json(characters);
+      })
+      .catch(err => {
+        res.status(500).json( { errorMessage: "Cannot find characters." })
+      })
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    Character.find({ _id: req.params.id })
+      .then(char => {
+        if(char.length > 0) {
+          res.status(201).json({ char });
+        } else {
+          res.status(404).json({ errorMessage: "The character with the specified ID does not exist." });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "Character information could not be retreived." });
+      })
+  })
+
+  router
+  .route('/:id/vehicle')
+  .get((req, res) => {
+    Character.find({ _id: req.params.id })
+      .then(char => {
+        if(char.length > 0) {
+          res.status(201).json({ char });
+        } else {
+          res.status(404).json({ errorMessage: "The character with the specified ID does not exist." });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "Character information could not be retreived." });
+      })
+  })
 
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,6 +4,29 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
+// const query = Film.find();
+// const producer = req.query.producer;
+// const released = req.query.released;
+
+
 // add endpoints here
+router
+  .route('/')
+  .get((req, res) => {
+    Film.find()
+    .sort('episode')
+    .then(films => {
+      res.status(200).json(films);
+    })
+    .catch(err => {
+      res.status(500).json( { errorMessage: "Server error.  Cannot find any films." })
+    })
+  })
+
+  // if(producer) {
+  //   query.where({ producer: producer });
+  // }
+
+  // query.then(films => res.json(films)).catch();
 
 module.exports = router;
