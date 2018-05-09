59562906ed3f315f47256f0cf4cbdb91a89836ae
cgoez <goez2001@gmail.com>
2018-05-09 16:06:26 -0700

    filmsRouter populate planets

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -10,14 +10,21 @@ const router = express.Router();
 router.route("/").get((req, res) => {
   Film.find({})
     .sort({ episode: 1 })
-    .populate('characters', {
-        _id: 1,
-        name: 1,
-        gender: 1,
-        height: 1,
-        skin_color: 1,
-        hair_color: 1,
-        eye_color: 1
+    .populate("characters", {
+      _id: 1,
+      name: 1,
+      gender: 1,
+      height: 1,
+      skin_color: 1,
+      hair_color: 1,
+      eye_color: 1
+    })
+    .populate("planets", {
+      name: 1,
+      climate: 1,
+      terrain: 1,
+      gravity: 1,
+      diameter: 1
     })
     .then(films => {
       res.status(200).json(films);
