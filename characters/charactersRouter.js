4a67f2e5e0c0aef9a83eda93dd587474fd2f55eb
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-09 15:41:28 -0700

    FindById for characterRouter.js

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,5 +5,45 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Character.findById(id).then(data => {
+        if (data.movies.length === 0) {
+            let addMovies = [];
+            Film.find({ characters: id })
+                .then(films => {
+                    addMovies = films.map(eachFilm => {
+                        return Character.findByIdAndUpdate(
+                            id,
+                            { $push: { movies: eachFilm._id } },
+                            { new: true }
+                        );
+                    });
+                });
+                .then(() => {
+                    Promise.all(addMovies).then(() => {
+                        Character.findById(id)
+                            .populate('movies', 'title -_id')
+                            .then(finalData => {
+                                res.send(finalData);
+                            });
+                    });
+                });
+              } else {
+                data
+                    .populate('movies', 'title')
+                    .execPopulate()
+                    .then(finalData => {
+                        res.send(finalData);
+                    });
+                }
+    });
+});
+
+router.get('/:id/vehicles')
+
+
+    
 
 module.exports = router;
