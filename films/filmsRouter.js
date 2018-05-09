71c12c167f70238e8568b68aac0444328c8ec28c
cgoez <goez2001@gmail.com>
2018-05-09 16:03:29 -0700

    filmsRouter populate characters

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -10,6 +10,15 @@ const router = express.Router();
 router.route("/").get((req, res) => {
   Film.find({})
     .sort({ episode: 1 })
+    .populate('characters', {
+        _id: 1,
+        name: 1,
+        gender: 1,
+        height: 1,
+        skin_color: 1,
+        hair_color: 1,
+        eye_color: 1
+    })
     .then(films => {
       res.status(200).json(films);
     })
