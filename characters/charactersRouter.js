4cab0c5ca9756e005a398757a50f0c147e340a4c
Devon Bacon <devon.bacon@gmail.com>
2018-05-09 12:49:14 -0700

    Implemented get request in character router

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,5 +5,17 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
+router.route('/')
+  .get((req, res) => {
+    Character.find()
+      .then(characters => {
+        res.status(200).json(characters);
+      })
+      .catch(err => {
+        res.status(500).json({
+          err: "Characters cannot be retrieved"
+        })
+      })
+  })
 
 module.exports = router;
