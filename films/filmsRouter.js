74c5ac9111d7749ebf8b64f855203a929936d85a
Mark Wong <fmarkwong@gmail.com>
2018-05-09 16:06:21 -0700

    WIP

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,4 +6,16 @@ const router = express.Router();
 
 // add endpoints here
 
+router.get('/', (req, res) => {
+  Film.findById('5aa995c2b97194b732c16802')
+    // .sort('episode')
+    .populate('characters')
+    .then(films => {
+      res.status(200).json(films);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
