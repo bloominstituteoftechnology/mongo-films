8e53bdd5e12e576e7fbb4fde1fa90817cc56d0f6
axhon <axhonruiz@gmail.com>
2018-05-09 11:30:38 -1000

    added tallgirls route

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -6,6 +6,15 @@ const Vehicle = require('../vehicles/Vehicle.js')
 
 const router = express.Router()
 
+router.get('/', (req, res, next) => {
+  const { minheight } = req.query
+  Character.find({ gender: 'female' })
+    .then(tallgirls => {
+      res.json(tallgirls.filter(girl => girl.height > minheight))
+    })
+    .catch(err => next(err))
+})
+
 // Fetch all vehicles for character with given id
 router.get('/:id/vehicles', (req, res, next) => {
   Vehicle.find({ pilots: req.params.id })
@@ -26,6 +35,5 @@ router.use('/:id', (req, res, next) => {
     })
     .catch(err => next(err))
 })
-
 
 module.exports = router
