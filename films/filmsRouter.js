a09a435002c175b78791bff1733c46e857f18b88
Chris <cjmarsh725@gmail.com>
2018-05-09 11:53:40 -0700

    Completed film route

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,6 +4,24 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
-// add endpoints here
+router.get('/', (req, res) => {
+  const producer = req.query.producer;
+  const released = req.query.released;
+  let query;
+  if (producer) {
+    query = Film.find().where({producer: producer});
+  } else if (released) {
+    query = Film.find().where({release_date: {$regex: released}});
+  } else {
+    query = Film.find().sort('episode');
+  }
+  query
+    .then(films => {
+      res.status(200).json(films);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
