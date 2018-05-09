877c2d34f35260e1cb9c09ba7219286daec5066a
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-09 14:42:11 -0700

    Sort by Episode & Populate Character & Planets

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,15 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res => {
+    const producerFilter = req.query.producer;
+    const filmReleased = req.query.released;
+
+    let query = Film.find({}).sort('episode')
+        .populate('characters', 'name gender height skin_color hair_color eye_color')
+        .populate('planets', 'name climate terrain gravity diameter');
+
+
+});
 
 module.exports = router;
