fbbc71274b5ca49382cf7ea7ea75996c97b739f9
Jacob Leon Lyerla <umasour@yahoo.com>
2018-05-09 15:58:56 -0700

    Crud endpoints working
    except post for all my Router.js files

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -5,5 +5,66 @@ const Planet = require('./Planet.js');
 const router = express.Router();
 
 // add endpoints here
+router
+  .route('/')
+  .get(get)
+  .post(post);
+
+router
+  .route('/:id')
+    .get((req, res) => {
+        const {id} = req.params
+    Planet
+    .findById(id)
+    .then(Planet=>{
+           res.status(202).json(Planet);
+    })
+    .catch(err=>{
+        res.status(500).json({errorMessage: "The Planets information could not be retrieved."})
+    })
+  })
+  .delete((req, res) => {
+    const {id} = req.params
+    Planet
+    .findByIdAndRemove(id)
+    .then(Planet=>{
+        res.status(204).end()
+    })
+  })
+  .put((req, res) => {
+    const {id} = req.params
+    const update = req.body;
+    const options ={
+      new:true,
+    }
+    Planet
+    .findByIdAndUpdate(id,update, options)
+    .then(Planet=>{
+      res.status(200).json(Planet)
+        })
+  });
+
+function get(req, res) {
+  Planet.find().then(Planets => {
+    res.status(200).json(Planets);
+  })
+  .catch(err=>{
+      res.status(500).json({errorMessage: "The Planets information could not be retrieved."})
+  })
+}
+
+function post(req, res) {
+
+  const planet = new Planet(req.body);
+console.log(planet)
+  planet
+    .save()
+    .then(c => {
+      res.status(201).json(c);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: `${planet}` });
+    });
+}
 
 module.exports = router;
--- species/speciesRouter.js
+++ species/speciesRouter.js
@@ -5,5 +5,66 @@ const Specie = require('./Specie.js');
 const router = express.Router();
 
 // add endpoints here
+router
+  .route('/')
+  .get(get)
+  .post(post);
+
+router
+  .route('/:id')
+    .get((req, res) => {
+        const {id} = req.params
+    Specie
+    .findById(id)
+    .then(Specie=>{
+           res.status(202).json(Specie);
+    })
+    .catch(err=>{
+        res.status(500).json({errorMessage: "The Species information could not be retrieved."})
+    })
+  })
+  .delete((req, res) => {
+    const {id} = req.params
+    Specie
+    .findByIdAndRemove(id)
+    .then(Specie=>{
+        res.status(204).end()
+    })
+  })
+  .put((req, res) => {
+    const {id} = req.params
+    const update = req.body;
+    const options ={
+      new:true,
+    }
+    Specie
+    .findByIdAndUpdate(id,update, options)
+    .then(Specie=>{
+      res.status(200).json(Specie)
+        })
+  });
+
+function get(req, res) {
+  Specie.find().then(Species => {
+    res.status(200).json(Species);
+  })
+  .catch(err=>{
+      res.status(500).json({errorMessage: "The Species information could not be retrieved."})
+  })
+}
+
+function post(req, res) {
+
+  const specie = new Specie(req.body);
+
+  specie
+    .save()
+    .then(c => {
+      res.status(201).json(c);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: `${specie}` });
+    });
+}
 
 module.exports = router;
