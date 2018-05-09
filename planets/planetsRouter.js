97571785145bc239e82a07bc1d9418baa3044212
Henry K <mykim1891@hotmail.com>
2018-05-09 14:12:56 -0700

    All routers have valid endpoint

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -5,5 +5,14 @@ const Planet = require('./Planet.js');
 const router = express.Router();
 
 // add endpoints here
+router.get("/", (req, res) => {
+  Planet.find()
+    .then(planet => {
+      res.status(201).json(planet);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
--- starships/starshipsRouter.js
+++ starships/starshipsRouter.js
@@ -5,5 +5,14 @@ const Starship = require('./Starship.js');
 const router = express.Router();
 
 // add endpoints here
+router.get("/", (req, res) => {
+  Film.find()
+    .then(starship => {
+      res.status(201).json(starship);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
