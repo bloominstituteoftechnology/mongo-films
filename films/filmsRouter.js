e477ef256589b5e17074fafb798e1166d2fb0394
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-09 15:56:31 -0400

    get films by producer or release date

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -1,9 +1,25 @@
 const express = require('express');
-
 const Film = require('./Film.js');
-
 const router = express.Router();
 
-// add endpoints here
+router.route('/')
+  .get((req, res) => {
+    const { producer, released } = req.query;
+
+    // /api/films?producer=gary+kurtz
+    if (producer) Film.find({}).where({ producer: new RegExp(producer, 'i') })
+      .then(films => res.json(films))
+      .catch(err => res.json(`Cannot find and films produced by ${producer}`));
+    
+    // /api/films?released=2005
+    if (released) Film.find({}).where({ release_date: new RegExp(released, 'i') })
+      .then(films => res.json(films))
+      .catch(err => res.json(`Cannot find any films released in ${released}`));
+
+    Film.find()
+      .then(films => res.json(films))
+      .catch(err => res.json("There was an error."))
+  })
+
 
 module.exports = router;
