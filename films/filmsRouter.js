4c97a3e93fce3cc2709adbca170409da07284a6a
Devon Bacon <devon.bacon@gmail.com>
2018-05-09 13:00:30 -0700

    Added get request to films router

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,17 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.route('/')
+  .get((req, res) => {
+    Film.find()
+      .then(films => {
+        res.status(200).json(films);
+      })
+      .catch(err => {
+        res.status(500).json({
+          err: "Films cannot be retrieved"
+        })
+      })
+  })
 
 module.exports = router;
