da3b011c17e4aca5d6aaaf15228a88a503bfeddd
Jared Cuffe <jcuffe@gmail.com>
2018-05-09 12:35:26 -0700

    GET all films implemented

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,10 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.use('/', (req, res, next) => {
+  Film.find().populate(['starships', 'vehicles', 'characters', 'species', 'planets'])
+    .then(films => res.send(films))
+    .catch((err) => next(err))
+})
 
 module.exports = router;
