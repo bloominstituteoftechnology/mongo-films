555fad02458341366f8e1481a8723ac8a147cddf
totsuya <supertatortots@gmail.com>
2018-05-10 16:32:45 -0400

    added vehicles .get to characterRouter

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const Character = require('./Character.js');
 
+const Vehicles = require('../vehicles/Vehicle.js');
+
 const router = express.Router();
 
 // add endpoints here
@@ -21,7 +23,19 @@ router.get('/:id', (req, res) => {
   .then(character => res.status(200).json(character))
   .catch(error => {
     res.status(500).json(error);
-  });
-});
+  })
+})
+
+router.get('/:id/vehicles', (req, res) => {
+const { id } = req.params;
+Vehicles.find({ pilots: id })
+.populate('pilots')
+.then(vehicle => {
+  res.status(200).json(vehicle);
+})
+.catch(error => {
+  res.status(500).json(err);
+  })
+})
 
 module.exports = router;
