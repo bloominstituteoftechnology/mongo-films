292b12ba252cd172189ba99092ba8c85d583f1bd
totsuya <supertatortots@gmail.com>
2018-05-10 18:14:17 -0400

    added basic vehicle endpoint

--- vehicles/vehiclesRouter.js
+++ vehicles/vehiclesRouter.js
@@ -5,5 +5,14 @@ const Vehicle = require('./Vehicle.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
+  Vehicle.find()
+    .then(vehicle => {
+      res.status(200).json(vehicle);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Could not retrieve data!' })
+    })
+})
 
 module.exports = router;
