ce0ce4f80fb773ffbc353df7658af0c6cfac2c47
nickbrenn <nbren@tutanota.com>
2018-05-10 13:59:41 -0400

    Made a method that returns only one character with films listed, so it works almost

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,14 +1,40 @@
 const express = require("express");
 
 const Character = require("./Character.js");
+const Film = require("../films/Film.js");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   Character.find()
     .populate("homeworld", "name climate terrain gravity diameter")
-    .then(characters => {
-      res.status(200).json(characters);
+    // .then(characters => {
+    //   // this version would need a map promise.all or something liek
+    //   res.status(200).json(characters);
+    // })
+    // below is the promise all version that works but only returns one
+    // .then(chars => {
+    //   const promises = chars.map(char => {
+    //     return Film.find({ characters: char._id })
+    //       .select("title")
+    //       .then(films => {
+    //         const character = { ...char._doc, movies: films };
+    //         // you can put return on bottom or remove the res status thing to change
+    //         res.status(200).json(character);
+    //       });
+    //   });
+    //   Promise.all(promises).then(chars => res.status(200).json(promises));
+    .then(chars => {
+      const promises = chars.map(char => {
+        return Film.find({ characters: char._id })
+          .select("title")
+          .then(films => {
+            const character = { ...char._doc, movies: films };
+            // you can put return on bottom or remove the res status thing to change
+            res.status(200).json(character);
+          });
+      });
+      Promise.all(promises).then(chars => res.status(200).json(promises));
     })
     .catch(err => {
       res.status(500).json({ errorMessage: "Could not get characters." });
@@ -19,8 +45,13 @@ router.get("/:id", (req, res) => {
   const id = req.params.id;
   Character.findById(id)
     .populate("homeworld", "name climate terrain gravity diameter")
-    .then(character => {
-      res.status(200).json(character);
+    .then(char => {
+      Film.find({ characters: id })
+        .select("title")
+        .then(films => {
+          const character = { ...char._doc, movies: films };
+          res.status(200).json(character);
+        });
     })
     .catch(err => {
       res.status(500).json({ errorMessage: "Could not get character." });
