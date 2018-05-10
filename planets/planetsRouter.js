e191a3ed561cfbf9e7b215ba663412612410fe7d
cgoez <goez2001@gmail.com>
2018-05-10 14:20:40 -0700

    planetsRouter GET /:id native species

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const Planet = require("./Planet.js");
 const Character = require("../characters/Character.js");
+const Specie = require("../species/Specie.js");
 
 const router = express.Router();
 
@@ -21,11 +22,18 @@ router.route("/").get((req, res) => {
 // GET /:id ; find all characters born in that planet and all native species
 router.route("/:id").get((req, res) => {
   const { id } = req.params;
+  let planetSpecies;
+
+  Specie.find({})
+    .where({ homeworld: id })
+    .then(species => {
+      planetSpecies = species.map(specie => specie.name);
+    });
 
   Character.find({})
     .where({ homeworld: id })
     .then(planets => {
-      res.status(200).json(planets);
+      res.status(200).json({ planets, native_species: planetSpecies });
     })
     .catch(error => {
       res.status(500).json(error);
