3428e0b061f6524f1644b8ffdc1c191d3b5eeb6c
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-05-10 16:47:18 -0400

    finished models, (i think) added basic route to character, added queries for films

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,5 +5,25 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', function(req, res) {
+  Character.find()
+    .populate(
+      'characters',
+      '_id name gender height skin_color hair_color eye_color species'
+    )
+    .then(chars => res.status(200).json(chars))
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+router.get('/:id', function(req, res) {
+  Character.findById(req.params.id)
+    .populate('homeworld')
+
+    .then(char => res.status(200).json(char))
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
