938c548e6df747f4ce7051cf39be8faaebedd5d6
David Alexander Morales <damorales89@gmail.com>
2018-05-10 17:22:20 -0400

    some routers done

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,28 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', function (res, req) {
+    let query = Film
+        .find()
+        .select('episode producer title director release_date')
+        .populate('planets', 'name climate terrain gravity diameter')
+        .populate(
+            'characters',
+            'name gender height skin_color hair_color eye_color'
+        );
+    
+    const { producer, released } = req.query;
+
+    if (producer) {
+        const filter = new RegExp(producer, 'i');
+        query.where({ producer: filter });
+    }
+
+    if (released) {
+        query.where({ release_date: { $regex: released, $options: 'i'}});
+    }
+
+    query.then(films => res.status(200).json(films));
+});
 
 module.exports = router;
