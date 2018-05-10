60b3ceee8b61a050bd6ff61a662667733f0041b2
Michal Chmielniak <michal@Michals-MacBook-Pro.local>
2018-05-10 16:55:07 -0400

    fixed planets

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
       res.status(200).json(planets);
     })
     .catch(err => {
-      res.status(500).json({ errorMessage: "Could not get planets." });
+      res.status(500).json({ errorMessage: "No Planets Maybe Destroyed By DeathStar" });
     });
 });
 
