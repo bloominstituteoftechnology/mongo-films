cf68efb325d3e86d9772cb06aac0c879623b7ff7
Yasin Shuman <email@yasin.io>
2018-05-10 16:09:28 -0700

    completed end point retrieving characters vehicles

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.route("/").get(get);
 
 const Film = require("../films/Film");
+const Vehicle = require("../vehicles/Vehicle");
 
 //returns a list of all characters
 function get(req, res) {
@@ -36,6 +37,20 @@ router.route("/:id").get((req, res) => {
     .catch(err => {
       console.log("we have an error: ", err);
       res.status(500).json(err);
+    });
+});
+
+router.get("/:id/vehicles", (req, res) => {
+  const { id } = req.params;
+
+  Vehicle.find({ pilots: id }, "vehicle_class")
+    // .populate("vehicle")
+    .then(docs => {
+      res.status(200).json(docs);
+    })
+    .catch(err => {
+      console.log("we have an error: ", err);
+      res.status(404).json(err);
     });
 });
 
