52670a19b9e27eafaa9623b53197692d0a2df774
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-10 12:13:58 -0700

    Populate Characters with a particular Species

--- species/speciesRouter.js
+++ species/speciesRouter.js
@@ -1,9 +1,28 @@
 const express = require('express');
 
-const Specie = require('./Specie.js');
+const Species = require('./Specie.js');
+const Characters = require('../characters/Character.js');
 
 const router = express.Router();
 
 // add endpoints here
+router.put('/populate/characters', (req, res) => {
+    let flag:
+    Species.find()
+        .then(species => {
+            species.forEach(each => {
+                if (each.characters.length > 0) flag = true;
+            });
+        })
+        .then(() => {
+            if (flag) res.send('Characters are already populated.');
+            else {
+                const findSpecies = Species.find();
+                const findCharacters = Characters.find();
+
+                Promise.all([findSpecies, findCharacters]).then(results)
+            }
+        })
+})
 
 module.exports = router;
