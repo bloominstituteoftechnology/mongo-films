fdd0457855e7ae98076f8634c0bf6835971217be
totsuya <supertatortots@gmail.com>
2018-05-10 17:01:25 -0400

    added basic get route for species Router

--- species/speciesRouter.js
+++ species/speciesRouter.js
@@ -5,5 +5,16 @@ const Specie = require('./Specie.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
+  Specie.find()
+    .then(species => {
+      res.status(200).json(species);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: 'Species could not be retrieved!'
+      });
+    })
+})
 
 module.exports = router;
