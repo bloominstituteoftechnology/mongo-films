94e347144dc986dcdfc27ac0a01030fb2c72664d
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-10 12:05:00 -0700

    Updated Species.js with pluralization

--- species/Species.js
+++ species/Species.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const Specie = new mongoose.Schema({
+const Species = new mongoose.Schema({
   edited: { type: Date, default: Date.now },
   classification: String,
   name: { type: String, required: true, index: true },
@@ -23,4 +23,4 @@ const Specie = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('Specie', Specie);
+module.exports = mongoose.model('Species', Species);
