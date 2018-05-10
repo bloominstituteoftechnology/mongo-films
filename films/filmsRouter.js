afb63892c33d6d991922c47746208c950705afe6
Justin Hong <jhong2009@gmail.com>
2018-05-10 12:51:38 -0700

    in Film.js: edited references to be collections not single objects. in filmRouter.js: created a query to sort film data and populated characters and planets

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,7 +6,21 @@ const router = express.Router();
 
 // add endpoints here
 router.route("/").get((req, res) => {
-	Film.find()
+	// define query - what we are searching for
+	let query = Film.find()
+		// filter out unneccesary ids
+		.select("-planet_ids -character_ids -specie_ids -starship_ids -vehicle_ids")
+		// order episodes from smallest to largest
+		.sort("episode")
+		// replace character ObjectIds with character data
+		.populate(
+			"characters",
+			"name gender height skin_color hair_color eye_color"
+		)
+		// replace planet ObjectIds with planet data
+		.populate("planets", "name climate terrain gravity diameter");
+
+	query
 		.then(films => {
 			res.status(200).json(films);
 		})
