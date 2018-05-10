63c33c92f81f98b63eb02d919198fab5a793c52c
Mark Wong <fmarkwong@gmail.com>
2018-05-09 23:17:08 -0700

    produced and released filter

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -7,13 +7,23 @@ const router = express.Router();
 // add endpoints here
 
 router.get('/', (req, res) => {
-  Film.find({})
+  const query = Film.find({})
     .sort('episode')
     .populate('characters', '__id name gender height, skin_color, hair_color, eye_color')
-    .populate('planets', 'name climate terrain gravity diameter')
-    .then(films => {
-      res.status(200).json(films);
-    })
+    .populate('planets', 'name cthenlimate terrain gravity diameter');
+
+  if (req.query.producer) {
+    query.regex('producer', new RegExp(req.query.producer, 'i'));
+  }
+
+  if (req.query.released) {
+    query.regex('release_date', new RegExp(req.query.released));
+  }
+
+  query.exec((err, films) => {
+    if (err) return console.log(err);
+    res.status(200).json(films);
+  })
 });
 
 module.exports = router;
