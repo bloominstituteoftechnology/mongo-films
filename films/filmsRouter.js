cb85b1b1c30ab2867e5efae14b41ced9f6920dbe
odax <nr10@albion.edu>
2018-05-10 17:14:58 -0400

    started working on routes

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,6 +4,42 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
-// add endpoints here
+router.get('/', function(req, res) {
+    Film
+    .find()
+    .then(films => {
+        res.status(200).json(films);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
 
+// add endpoints here /api/films?released=2005&producer=gary kurtz
+// router.get('/' , function(req, res) {
+//     Film.find()
+//     .select('producer, release_date')
+//     .then(films => res.status(200).json(films));
+// });
+
+// router.get('/' , function(req, res) {
+//     let query = Film.find().select('producer, release_date');
+//     const { producer, released } = req.query;
+//     if(producer) {
+//         //filterby producer, if i know it will always be gary curtz i can find and do that here, but i prob will be goot at regex here
+//         //it will be to make sure gary kurtz is in here
+//         const filter = new RegExp(producer, 'i') //i means case insensitive
+//         query.where({ producer: filter}) //this is dynamic it will search producer for gary if it is like this:   query.where({ producer: /gary/}) 
+//     }
+
+//     if (released) {
+//         query.where({release_date: { $regex: released, $options: 'i'}});
+//     }
+
+//     query.then(films => res.status(200).json(films));
+// });
+
+// test: localhost:5000/api/films?released=1977
+// or films?producer=gary kurtz
+//combine them!! localhost:5000/api/films?producer=gary kurtz&released=80
 module.exports = router;
--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,9 +1,27 @@
 const express = require('express');
 
 const Planet = require('./Planet.js');
+const Character = require('../characters/Character');
+const Species = require('../species/Specie');
+
 
 const router = express.Router();
 
 // add endpoints here
+router.get(':/id', function(req, res) {
+    const { id } = req.params;
+    const chars = Character.find({ homeworld: id });
+    const species = Character.find({ homeworld: id });
+//these return as promises
+    Promise.all([chars, species]).then(results => {
+        const [characters, species] = results;
+        //same as const characters = restults[0]
+
+        res.status(200).json({ characters, species });
+    })
+    .catch(err => res.send(err));
+});
+
+//localhost:5000/api/planets/planet id
 
 module.exports = router;
