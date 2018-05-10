e7dc8470c3d3766d82f2e96aef76034c5dea42ad
Eric Gomez <eric_gomez_1@hotmail.com>
2018-05-10 14:28:16 -0700

    comepleted all query requirements, knocked out a few end points, worked on some error handling

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -8,12 +8,16 @@ const router = express.Router();
 router.get('/', (req, res) => {
   let producer = req.query.producer === undefined ? null : req.query.producer;
   let release = req.query.producer === undefined ? null : req.query.released;
-  let query = Film.find();
+  let query = Film.find().select('producer episode release_date title director')
+    .populate('characters', 'name gender height skin_color hair_color eye_color')
+    .populate('planets', 'name climate terrain gravity diameter')
   if (producer) {
-    query.where('producer').equals(producer);
+    const filter = new RegExp(producer, 'i');
+    query.where({producer: filter})
   }
   if (release) {
-    query.where('release_date').equals(release);
+    const filter = new RegExp(released, 'i');
+    query.where({release_date: filter})
   }
   query.sort('episode').then(response => {
     res.json(response)
--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,9 +1,28 @@
 const express = require('express');
 
 const Planet = require('./Planet.js');
+const Character = require('../characters/Character.js');
 
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
+  Planet.find()
+    .then(response => {
+      res.json(response);
+    })
+})
 
+router.get('/:id', (req, res) => {
+  const {id} = req.params;
+  Planet.findById(id)
+    .then(planet => {
+      Character.find()
+      .where('homeworld').equals(id)
+      .then(response => {
+        console.log(response);
+        res.json({...planet._doc, characters: [...response]});
+      })
+    })
+})
 module.exports = router;
