77e81098156f7ba1167d51182e97a7ae6b0ada1b
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-10 18:48:03 -0400

    get planet by id with corresponding characters and species

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Planet = require('./Planet.js');
+const Character = require('../characters/Character');
+const Species = require('../species/Specie');
 const router = express.Router();
 
 router.route('/')
@@ -8,5 +10,30 @@ router.route('/')
       .then(planets => res.json(planets))
       .catch(err => res.json("Cannot fetch planets."))
   })
+
+router.route('/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+
+    const chars = Character.find({ homeworld: id }).select('name'); // returns a promise
+    const species = Species.find({ homeworld: id }).select('name'); // returns a promise
+
+    Planet
+      .findById(id)
+      .then(planet => {
+        Promise.all([chars, species])
+          .then(result => {
+            const [ characters, species ] = result; // destructuring the result array
+            const fullPlanet = { 
+              ...planet._doc, // retrieves document for the planet
+              charactersFromHere: characters, 
+              species: species 
+            };
+            res.status(200).json(fullPlanet);
+          })
+      })
+      .catch(err => res.status(500).json("Error."))
+    
+  }); // end of get
 
 module.exports = router;
