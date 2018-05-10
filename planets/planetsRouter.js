16cb2031e035dc3df4ecf833c5596a16488044dd
Nedu Robert <nedumrobert@gmail.com>
2018-05-10 22:05:41 +0100

    created endpoint for planet schema

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,9 +1,27 @@
 const express = require('express');
 
+const Character = require('../characters/Character');
+const Specie = require('../species/Specie');
 const Planet = require('./Planet.js');
 
 const router = express.Router();
 
 // add endpoints here
+//Given a planet Id find all characters born in that planet and all native species. (/api/planet/:id)
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const characters = Character.find({ homeworld: id });
+    const species = Specie.find({ homeworld: id });
+
+    Promise.all([characters, species])
+    .then (res => {
+        const [characters, species] = res;
+        res.status(200).json({ characters, species })
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
 
 module.exports = router;
