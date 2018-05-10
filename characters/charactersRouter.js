60f5b4ee49484a8b3fd1a8abec75a3244a0f324c
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-10 16:25:02 -0400

    Added movies property to character

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,19 +1,25 @@
 const express = require('express');
 
 const Character = require('./Character.js');
+const Film = require('../films/Film.js')
 
 const router = express.Router();
-const Film = require('../films/Film.js')
 
 // add endpoints here
 router.get('/:id', (req, res) => {
     const id = req.params.id;
 
-    let query = Character.find({ _id: id } ).populate("homeworld")
+    let query = Character.findById(id).populate("homeworld")
 
     query
-    .then(chars => {
-        res.json(chars)
+    .then(char => {
+        Film
+        .find({ characters: id })
+        .select('title')
+        .then(films => {
+            const character = { ...char._doc, movies: films }
+            res.json(character)
+        })
     })
     .catch(err => {
         res.json(err)
