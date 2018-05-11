724c80082b677fd7527c209a08006cc97579bb2e
JulianAlexander <julian.pineyro@gmail.com>
2018-05-11 17:44:35 +0300

    Project finished. Stretch started

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,4 +6,32 @@ const router = express.Router();
 
 // add endpoints here
 
+router.get('/', (req, res) => {
+    const producerFilter = req.query.producer;
+    const releaseFilter = req.query.released;
+  
+    let query = Film.find()
+        .select('title producer')
+        .populate('characters', '_id name gender height skin_color hair_color eye_color')
+        .populate('planets', 'name climate terrain gravity diameter')
+        .sort({ episode: "asc" });
+  
+    if (producerFilter) {
+      const filter = new RegExp(producerFilter, 'i');
+      query.where({ producer: filter });
+    }
+    if (releaseFilter) {
+      const filter = new RegExp(releaseFilter, 'i');
+      query.where({ release_date: filter });
+    }
+    query
+      .then(films => {
+        res.send(films);
+      })
+      .catch(err => {
+        res.status(400).send({ error: err });
+      });
+  });
+  
+
 module.exports = router;
