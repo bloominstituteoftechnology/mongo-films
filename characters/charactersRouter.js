1ab6ed6c7f0a68c4ac3409b3ff386ccd1a80759c
Shawn Stewart <s.stewart0220@gmail.com>
2018-05-10 19:14:33 -0500

    Completed specified character endpoints.

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,43 +1,50 @@
 const express = require('express');
 
 const Character = require('./Character.js');
+const Film = require('../films/Film');
+const Vehicle = require('../vehicles/Vehicle');
 
 const router = express.Router();
 
 // add endpoints here
-router
-  .route('/')
-  .get(get)
-  .post(post);
-
-router
-  .route('/:id')
-  .get((req, res) => {
-    res.status(200).json({ route: '/api/characters/' + req.params.id });
-  })
-  .delete((req, res) => {
-    res.status(200).json({ status: 'implementation in progress...' });
-  })
-  .put((req, res) => {
-    res.status(200).json({ status: 'implementation in progress...' });
-  });
-
-function get(req, res) {
-  Character.find().sort('name').select('name').then(chars => res.json(chars)).catch(err => res.status(500).json(err));
-}
-
-function post(req, res) {
-  const characterData = req.body;
-
-  const character = new Character(characterData);
-  character
-    .save()
-    .then(char => {
-      res.status(201).json(char);
-    })
+router.get('/', function(req, res) {
+  Character
+    .find()
+    .then(chars => res.json(chars))
     .catch(err => {
       res.status(500).json(err);
     });
-};
+});
+
+router.get('/:id', function(req, res) {
+  const { id } = req.params;
+
+  Character.findById(req.params.id)
+    .populate('homeworld', 'climate name -_id')
+    .then(char => {
+      Film
+        .find({ characters: id })
+        .select('title -_id')
+        .then(films => {
+          const character = {...char._doc, movies: films}
+          res.status(200).json(character);
+        });
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    });
+});
+
+router.get('/:id/vehicles', function(req, res) {
+  const { id } = req.params;
+
+  Vehicle
+    .find({ pilots: id })
+    .select('-pilot_keys -pilots')
+    .then(vehicles => res.json(vehicles))
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
