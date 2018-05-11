636d679e361456da30caf4c6b55fd86adfdf5ff0
David Alexander Morales <damorales89@gmail.com>
2018-05-10 22:39:56 -0400

    species added to planetrouter

--- species/speciesRouter.js
+++ species/speciesRouter.js
@@ -1,37 +1,20 @@
 const express = require('express');
 
+const Planet = require('../planets/Planet');
 const Specie = require('./Specie.js');
 
 const router = express.Router();
 
 // add endpoints here
 router.get('/', (req, res) => {
+
     Specie
     .find()
-    .then(species => res.status(200).json(species))
+    .then(species => res.status(200).json(species)
+    )
     .catch(err => {
         res.status(500).json(err);
     });
 });
-
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-
-    Specie
-        .findById(id)
-        .populate('homeworld', 'classification name')
-        .then(species => {
-            Specie.find({ specie: id })
-            .select('homeworld')
-            .then(homeworld => {
-                const planet = { ...planet._doc, homeworld: planet };
-                res.status(200).json(homeworld);
-
-            })
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        })
-})
 
 module.exports = router;
