362c74d35d7b7ceebc166db0050b63e8d2314b4d
Justin Hong <jhong2009@gmail.com>
2018-05-10 18:36:35 -0700

    set up react client project folders; installed react-router, react-router-dom, cors, material ui

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import "./styles/index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(
+	<Router>
+		<MuiThemeProvider>
+			<App />
+		</MuiThemeProvider>
+	</Router>,
+	document.getElementById("root")
+);
+registerServiceWorker();
