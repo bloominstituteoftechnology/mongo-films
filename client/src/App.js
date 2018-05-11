82d87bc07800088bd58c5bbbfad94fcb43036159
Justin Hong <jhong2009@gmail.com>
2018-05-10 18:51:25 -0700

    used cors to fetch characters data from db and save it in App.js state

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import axios from "axios";
 // material components
 import RaisedButton from "material-ui/RaisedButton";
 // styles
 import "./styles/App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      characters: []
+    };
+  }
+  // fetch list of characters from db
+  componentDidMount() {
+    this.getCharactersList();
+  }
+  getCharactersList() {
+    axios
+      .get("http://localhost:5000/api/characters")
+      .then(response => {
+        this.setState({ characters: response.data });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
   render() {
+    console.log(this.state.characters);
     return (
       <div className="App">
         <header className="App-header">
