8bcd18864c8e518560499deea9dd2f013b54f3ac
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-05-10 21:15:05 -0500

    not 100% but getting there/need to finish a few populates

--- films/Film.js
+++ films/Film.js
@@ -5,24 +5,24 @@ const Film = new mongoose.Schema({
   created: { type: Date, default: Date.now },
   episode: Number,
   edited: { type: Date, default: Date.now },
-  planet_ids:  [{type: ObjectId, ref: 'Planet'}],
+  planet_ids:  {type: ObjectId, ref: 'Planet'},
   producer: String,
   title: { type: String, required: true },
   director: String,
   release_date: String,
   opening_crawl: String,
-  character_ids: [{type: ObjectId, ref: 'Character'}],
-  specie_ids: [{type: ObjectId, ref: 'Species'}],
+  character_ids: {type: ObjectId, ref: 'Character'},
+  specie_ids: {type: ObjectId, ref: 'Species'},
   key: { type: Number, unique: true },
-  starship_ids: [{type: ObjectId, ref: 'Starship'}],
-  vehicle_ids: [{type: ObjectId, ref: 'Vehicle'}],
+  starship_ids: {type: ObjectId, ref: 'Starship'},
+  vehicle_ids: {type: ObjectId, ref: 'Vehicle'},
   // add fields for starships, vehicles, planets, characters and species
   // to link them to the corresponding model
-  starships: [{type: ObjectId, ref: 'Starship'}],
+  starships: {type: ObjectId, ref: 'Starship'},
   vehicle: [{type: ObjectId, ref: 'Vehicle' }],
-  planet: [{type: ObjectId, ref: 'Planet'}],
-  character: [{type: ObjectId, ref: 'Character'}],
-  species: [{type: ObjectId, ref: 'Species'}]
+  planets: [{type: ObjectId, ref: 'Planet'}],
+  characters: [{type: ObjectId, ref: 'Character'}],
+  species: {type: ObjectId, ref: 'Specie'}
 });
 
 
--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -4,6 +4,18 @@ const Planet = require('./Planet.js');
 
 const router = express.Router();
 
-// add endpoints here
+router.get('/', function get(req, res) {
+    Planet.find()
+     .then(planets =>
+      res.status(200).json(planets));
+    });
 
+
+  router.get('/:id', function get(req, res) {
+    Planet.findById(req.params.id)
+    .then(planet => res.status(200).json(planet))
+    .catch(err => {
+        res.status(500).json(err)
+    });
+});
 module.exports = router;
