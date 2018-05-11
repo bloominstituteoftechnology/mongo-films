0c046ed648e2702232927829f62b0608b6dff8df
David Alexander Morales <damorales89@gmail.com>
2018-05-10 23:46:25 -0400

    done with base project - no stretch

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -2,17 +2,38 @@ const express = require('express');
 
 const Character = require('./Character.js');
 const Film = require('../films/Film');
+const Vehicle = require('../vehicles/Vehicle');
 
 const router = express.Router();
 
 // add endpoints here
 router.get('/', function (req, res) {
+
     Character
         .find()
         .then(chars => res.status(200).json(chars))
         .catch(err => {
             res.status(500).json(err);
         });
+
+    const minheight = req.query.height;
+
+    const query = Character
+        .find({ gender: 'female' });
+
+    if (minheight) {
+        const minfilter = new RegExp(minheight, 'i');
+        query.where({ minheight: minfilter })
+    }
+
+    query
+        .then(chars => 
+            res.status(200).json(chars)
+        )
+        .catch(err => 
+            res.status(500).json(err)
+        );
+
 });
 
 router.get('/:id', function(req, res) {
@@ -33,5 +54,40 @@ router.get('/:id', function(req, res) {
             res.status(500).json(err);
         });
 });
+
+router.get('/:id/vehicles', (req, res) => {
+    const { id } = req.params;
+
+    Character
+        .findById(id)
+        .then(char => {
+            Vehicle
+                .find({ pilots: id })
+                .select('vehicle_class')
+                .then(vehicles => res.status(200).json(vehicles))
+                .catch(err => res.status(500).json(err))
+        })
+        .catch(err => res.status(500).json(err))
+});
+
+// router.get('/', (req, res) => {
+//     const minheight = req.query.minheight;
+
+//     const query = Character
+//         .find({ gender: 'female' });
+
+//     if (minheight) {
+//         const minfilter = new RegExp(minheight, 'i');
+//         query.where({ minheight: minfilter })
+//     }
+
+//     query
+//         .then(chars => 
+//             res.status(200).json(chars)
+//         )
+//         .catch(err => 
+//             res.status(500).json(err)
+//         );
+// });
 
 module.exports = router;
--- starships/starshipsRouter.js
+++ starships/starshipsRouter.js
@@ -5,5 +5,13 @@ const Starship = require('./Starship.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
+    Starship
+        .find()
+        .then(ship => res.status(200).json(ship))
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
