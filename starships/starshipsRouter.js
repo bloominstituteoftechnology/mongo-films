fb32bd992309dad30b9e4c510e54398ce4bab9df
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-06 19:32:00 -0400

    - GET /starships and GET /starships/id routes created - added field to schema

--- starships/starshipsRouter.js
+++ starships/starshipsRouter.js
@@ -5,5 +5,23 @@ const Starship = require('./Starship.js');
 const router = express.Router();
 
 // add endpoints here
+router
+    .route('/')
+    .get((req, res) => {
+        Starship.find({})
+        .then(starShips => res.json(starShips))
+        .catch(err => res.status(500).json({error: err}))
+    })
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        const { id }  = req.params;
+        Starship.findById(id)
+        .populate('Character')
+        .then(foundStarShip => res.json(foundStarShip))
+        .catch(err => res.status(500).json({error: err}))
+    })
+
 
 module.exports = router;
