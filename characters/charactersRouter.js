5a1de2b3a0df93d3b3b911d4e1ddc88beff70e04
unknown <sam.park.email@gmail.com>
2018-06-06 17:15:09 -0400

    working routes, working on filtering correctly

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -6,4 +6,30 @@ const router = express.Router();
 
 // add endpoints here
 
+router
+    .route("/")
+    .get((req, res) => {
+       Character.find()
+       .then(characters => {
+           res.status(200).json(characters);
+       }) 
+       .catch(error => {
+           res.status(500).json({ error: error.message })
+       })
+    });
+
+router
+    .route("/:id")
+    .get((req, res) => {
+        const { id } = req.params;
+        Character.findById(id)
+        .populate('homeworld')
+        .then(charFound => {
+            res.status(200).json(charFound);
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message })
+        })
+    })
+
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -1,9 +1,38 @@
-const express = require('express');
+const express = require("express");
 
-const Film = require('./Film.js');
+const Film = require("./Film.js");
 
 const router = express.Router();
 
 // add endpoints here
+router.route("/").get((req, res) => {
+  Film.find()
+    .then(films => {
+      res.status(200).json(films);
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+router.route("/:id").get((req, res) => {
+  const { id } = req.params;
+  Film.findById(id)
+    .populate("characters", {
+      _id: 1,
+      name: 1,
+      gender: 1,
+      height: 1,
+      skin_color: 1,
+      hair_color: 1,
+      eye_color: 1
+    })
+    .then(foundFilm => {
+        res.status(200).json(foundFilm);
+    })
+    .catch(error => {
+        res.status(500).json({ error: error.message });
+    });
+});
 
 module.exports = router;
