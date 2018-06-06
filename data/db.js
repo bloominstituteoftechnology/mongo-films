65c196c13886e66f80e2d0762ec49c4bfc6c71ca
Lavell Burton <taylormade800@gmail.com>
2018-06-06 12:42:12 -0700

    added mongoimport to db for all json files

--- data/db.js
+++ data/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 module.exports = {
-  connectTo: function(database = 'sandbox', host = 'localhost') {
+  connectTo: function (database = 'sandbox', host = 'localhost') {
     return mongoose.connect(`mongodb://${host}/${database}`);
   },
 };
+
+// added mongoimport to db for all json files
