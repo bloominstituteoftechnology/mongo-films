b65cf782460f8c12f877e90093f5bff7b2b7c276
Zach Campbell <beazlebubbles@gmail.com>
2018-06-06 16:06:28 -0600

    Added character and planet routes

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -6,4 +6,25 @@ const router = express.Router();
 
 // add endpoints here
 
+router
+  .route('/')
+  .get((req, res) => {
+    Character.find()
+      .then(characters => {
+        res.json({ characters });
+      })
+      .catch(error => res.status(500).json({ error: 'Error fetching characters' }));
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    Character.findById(req.params.id)
+      .populate('homeworld')
+      .then(character => {
+        res.json({ character });
+      })
+      .catch(error => res.status(500).json({ error: 'Error fetching character' }));
+  });
+
 module.exports = router;
