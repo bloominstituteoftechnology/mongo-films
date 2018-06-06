3e7d2d33beee194bb9839132a8706a37d308e0e1
bbenefield89 <bsquared18@gmail.com>
2018-06-06 17:37:10 -0400

    feat(Film): add refs to `Character` && `Planets` feat(filmsRouter): `GET:/` returns all films feat(filmsRouter): `GET:/` sorts by episode number feat(filmsRouter): `GET:/` can return films depending on query params of either `api/films?producer=` or `api/films?released=`

--- routes/films/filmsRouter.js
+++ routes/films/filmsRouter.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const Film = require('./Film.js');
 
 const router = express.Router();
 
-// add endpoints here
+/*************************
+** ROUTE: / **
+*************************/
+// get
+router
+  .get('/', (req, res) =>  {
+    const producer = new RegExp(req.query.producer, 'i');
+    const released = (req.query.released) ? new RegExp('^' + req.query.released) : /\d+/;
+    
+    Film.find()
+      .where('producer', producer)
+      .where('release_date', released)
+      .sort('episode')
+      .populate('characters', 'name gender height skin_color hair_color eye_color')
+      .populate('planets', { _id: 0, name: 1, climate: 1, terrain: 1, gravity: 1, diameter: 1 })
+      .exec((err, raw) => {
+        if (err)
+          res.status(500).json(err);
+
+        res.json(raw);
+      });
+  });
 
 module.exports = router;
