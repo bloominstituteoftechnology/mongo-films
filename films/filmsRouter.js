49cb10c6f6fa2132185e33a28883823f64cb054a
Thuy <tpham045@hotmail.com>
2018-06-06 23:17:59 +0000

    feat: find films by producer name

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -8,15 +8,26 @@ const router = express.Router();
 router
     .route('/')
     .get((req, res) => {
+        if (req.query.producer) {
 
-        Film.find({})
-            .sort('episode')
-            .populate('characters', 'name gender height skin_color hair_color eye_color')
-            .populate('planets', 'name climate terrain gravity diameter')
-            .then(response => {
-                res.status(200).json({ data: response })
-            })
-            .catch(err => res.status(500).json({ data: err }))
+            Film.find({ producer: { "$regex": req.query.producer, "$options": "i" } })
+
+                .then(response => {
+                    res.status(200).json({ data: response })
+                })
+                .catch(err => res.status(500).json({ data: err }))
+        }
+        else {
+
+            Film.find({})
+                .sort('episode')
+                .populate('characters', 'name gender height skin_color hair_color eye_color')
+                .populate('planets', 'name climate terrain gravity diameter')
+                .then(response => {
+                    res.status(200).json({ data: response })
+                })
+                .catch(err => res.status(500).json({ data: err }))
+        }
     })
 
 module.exports = router;
