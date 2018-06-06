8685d3e708bf3e3e075a089ccb4654a8ba1dbcaa
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-06 16:36:31 -0400

    Created GET endpoints for species to test

--- species/speciesRouter.js
+++ species/speciesRouter.js
@@ -1,9 +1,31 @@
 const express = require('express');
-
 const Specie = require('./Specie.js');
-
+const sendErrorMessage = require('../helpers');
 const router = express.Router();
 
-// add endpoints here
+router
+  .get('/', (req, res) => {
+    Specie.find()
+      .then(species => {
+        res.status(200).json(species);
+      })
+      .catch(error => {
+        res.status(500).json({ error: 'The list of species could not be retrieved.' });
+      });
+  })
+  .get('/:id', (req, res) => {
+    const { id } = req.params;
+    Specie.findById(id)
+      .then(specie => {
+        if(specie){
+          res.status(200).json(specie);
+        } else {
+          res.status(404).json({ error: 'The specie with the specified ID does not exist.' });
+        }
+      })
+      .catch(error => {
+        sendErrorMessage(error, res, 'The specie could not be retrieved.');
+      });
+  })
 
 module.exports = router;
