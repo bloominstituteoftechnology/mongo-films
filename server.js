e15ad8aab31589d9483f18ff5c3271039d0d08d7
jesuarva <jean.ariza@jesuarva.com>
2018-06-07 00:07:35 +0200

    SET UP Dev Enviroment

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ const planetsRouter = require('./planets/planetsRouter.js');
 
 const server = express();
 
-db
-  .connectTo('starwars')
+db.connectTo('starwars')
   .then(() => console.log('\n... API Connected to Database ...\n'))
   .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
 
@@ -29,6 +28,4 @@ server.use('/api/planets', planetsRouter);
 server.get('/', (req, res) => res.send('API Running...'));
 
 const port = process.env.PORT || 5000;
-server.listen(port, () =>
-  console.log(`\n\nAPI running on http://localhost:${port}`)
-);
+server.listen(port, () => console.log(`\n\nAPI running on http://localhost:${port}`));
