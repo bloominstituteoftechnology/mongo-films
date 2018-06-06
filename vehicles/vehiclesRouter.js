056534adcd0b98ac30f6b934b08bdd7e1134f687
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-06 17:24:39 -0400

    Created GET endpoints for vehicles to test

--- vehicles/vehiclesRouter.js
+++ vehicles/vehiclesRouter.js
@@ -1,9 +1,31 @@
 const express = require('express');
-
 const Vehicle = require('./Vehicle.js');
-
+const sendErrorMessage = require('../helpers');
 const router = express.Router();
 
-// add endpoints here
+router
+  .get('/', (req, res) => {
+    Vehicle.find()
+      .then(vehicles => {
+        res.status(200).json(vehicles);
+      })
+      .catch(error => {
+        res.status(500).json({ error: 'The list of vehicles could not be retrieved.' });
+      });
+  })
+  .get('/:id', (req, res) => {
+    const { id } = req.params;
+    Vehicle.findById(id)
+      .then(vehicle => {
+        if(vehicle){
+          res.status(200).json(vehicle);
+        } else {
+          res.status(404).json({ error: 'The vehicle with the specified ID does not exist.' });
+        }
+      })
+      .catch(error => {
+        sendErrorMessage(error, res, 'The vehicle could not be retrieved.');
+      });
+  })
 
 module.exports = router;
