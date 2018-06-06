e70c58ffdc8069dfa6bade1fea76b08633f412e8
Thuy <tpham045@hotmail.com>
2018-06-06 22:13:54 +0000

    feat: list of filmss with characters info and planets info

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,6 +4,19 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
-// add endpoints here
+// get all list of films 1-orderBy episodes
+router
+    .route('/')
+    .get((req, res) => {
+
+        Film.find({})
+            .sort('episode')
+            .populate('characters', 'name gender height skin_color hair_color eye_color')
+            .populate('planets', 'name climate terrain gravity diameter')
+            .then(response => {
+                res.status(200).json({ data: response })
+            })
+            .catch(err => res.status(500).json({ data: err }))
+    })
 
 module.exports = router;
