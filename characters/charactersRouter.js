7754f72bc32568df7da6bb4fcdd8b52babadc302
Anne Courtney <annecourtney_1@yahoo.com>
2018-06-06 18:10:25 -0500

    finished characters, films, and planets.

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -4,6 +4,40 @@ const Character = require('./Character.js');
 
 const router = express.Router();
 
-// add endpoints here
+router
+  .route('/')
+  .get((req, res) => {
+
+    Character
+    .find()
+    .then(characters => {
+      res.status(200)
+      res.json({ characters })
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({ message: 'The characters information could not be retrieved.' });
+    })
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    const { id } = req.params; 
+
+    Character
+    .findById(id)
+    .populate('homeworld')
+    .populate('movies')
+    .then(character => {
+        res.status(200)
+        res.json({ character })
+    })
+    .catch(err => {
+        res.status(500)
+        res.json({ message: "The character information could not be retrieved." })
+    })
+  })
+
 
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,6 +4,44 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
-// add endpoints here
+router
+  .route('/')
+  .get((req, res) => {
+
+    Film
+    .find()
+    .then(films => {
+      res.status(200)
+      res.json({ films })
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({ message: 'The films information could not be retrieved.' });
+    })
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    const { id } = req.params; 
+
+    Film
+    .findById(id)
+    .populate('starships')
+    .populate('vehicles')
+    .populate('planets')
+    .populate('vehicles')
+    .populate('characters')
+    .populate('species')
+    .then(film => {
+        res.status(200)
+        res.json({ film })
+    })
+    .catch(err => {
+        res.status(500)
+        res.json({ message: "The film information could not be retrieved." })
+    })
+  })
+
 
 module.exports = router;
