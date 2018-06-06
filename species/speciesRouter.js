7207a3b059d139cb728514fa4b73932147c03122
Zach Campbell <beazlebubbles@gmail.com>
2018-06-06 16:09:12 -0600

    Added species route

--- species/speciesRouter.js
+++ species/speciesRouter.js
@@ -6,4 +6,27 @@ const router = express.Router();
 
 // add endpoints here
 
+
+router
+  .route('/')
+  .get((req, res) => {
+    Specie.find()
+      .then(species => {
+        res.json({ species });
+      })
+      .catch(error => res.status(500).json({ error: 'Error fetching species' }));
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    Specie.findById(req.params.id)
+      .populate("homeworld")
+      .then(specie => {
+        res.json({ specie });
+      })
+      .catch(error => res.status(500).json({ error: 'Error fetching specie' }));
+  });
+
+
 module.exports = router;
