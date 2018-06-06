ea33b47023e75bf9cb0d208384a6251958e33502
jaklundun <govi.escudero@gmail.com>
2018-06-06 15:03:10 -0700

    Configured relationships on the provided mongoose Schemas.

--- starships/Starship.js
+++ starships/Starship.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
+
 const Starship = new mongoose.Schema({
   pilot_keys: [Number],
   mglt: String,
@@ -8,6 +9,7 @@ const Starship = new mongoose.Schema({
   hyperdrive_rating: String,
   key: { type: Number, unique: true },
   // add pilots field to lik the ship to the characters model
+  pilots: [{type: ObjectId, ref: 'Character'}],
 });
 
 module.exports = mongoose.model('Starship', Starship);
