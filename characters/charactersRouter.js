8456d9d353a90ba01dc2273bc43d7da0329e87f9
Lavell Burton <taylormade800@gmail.com>
2018-06-06 13:43:58 -0700

    added first GET request with router

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -4,6 +4,17 @@ const Character = require('./Character.js');
 
 const router = express.Router();
 
-// add endpoints here
+// add endpoints here => root imports below
+router
+    .route('')
+    .get((req, res) => {
+        Character.find()
+            .then(character => {
+                res.status(200).json(character);
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'err' })
+            });
+})
 
 module.exports = router;
