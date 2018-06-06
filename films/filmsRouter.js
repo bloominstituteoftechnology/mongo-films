b9de0be55e3ff3c2b7a6f54c1889a6bd09abe50f
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-06 16:04:52 -0400

    Created GET endpoints for films to test

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -1,9 +1,31 @@
 const express = require('express');
-
 const Film = require('./Film.js');
-
+const sendErrorMessage = require('../helpers');
 const router = express.Router();
 
-// add endpoints here
+router
+  .get('/', (req, res) => {
+    Film.find()
+      .then(films => {
+        res.status(200).json(films);
+      })
+      .catch(error => {
+        res.status(500).json({ error: 'The list of films could not be retrieved.' });
+      });
+  })
+  .get('/:id', (req, res) => {
+  const { id } = req.params;
+  Film.findById(id)
+    .then(film => {
+      if(film){
+        res.status(200).json(film);
+      } else {
+        res.status(404).json({ error: 'The film with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      sendErrorMessage(error, res, 'The film could not be retrieved.');
+    });
+  })
 
 module.exports = router;
