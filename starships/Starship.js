a060c5e76e79affbe3a318e06aaf8bb4bae39c72
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-06 17:20:29 -0400

    Added pilots to Starship schema and linked to Character model

--- starships/Starship.js
+++ starships/Starship.js
@@ -7,7 +7,7 @@ const Starship = new mongoose.Schema({
   starship_class: String,
   hyperdrive_rating: String,
   key: { type: Number, unique: true },
-  // add pilots field to lik the ship to the characters model
+  pilots: [{ type: ObjectId, ref: 'Character' }]
 });
 
 module.exports = mongoose.model('Starship', Starship);
