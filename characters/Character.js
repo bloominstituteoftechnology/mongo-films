3317ec326591a511b350760a4fde70a407c99cf3
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-06 21:19:23 -0700

    movies property added to character schema

--- characters/Character.js
+++ characters/Character.js
@@ -13,7 +13,8 @@ const Character = mongoose.Schema({
   birth_year: String,
   key: { type: Number, unique: true },
   homeworld_key: Number,
-  homeworld: { type: ObjectId ,ref: "Planet"}
+  homeworld: { type: ObjectId ,ref: "Planet"},
+  movies: [{ type: ObjectId, ref: "Film"}]
 });
 
 module.exports = mongoose.model('Character', Character);
