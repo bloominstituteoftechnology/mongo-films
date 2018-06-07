cf2fcb21c9d2b03d3795393c9256492747ddfcb6
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-06-07 16:11:25 -0400

    films find query producer

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,24 +4,51 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
+
 // add endpoints here
 
 router
 .route('/')
 .get((req, res) => {
-  Film.find().sort( { episode: 1 } )
-    .populate('characters', 'name gender height skin_color hair_color eye_color')
-    .populate('planets', '-_id name climate terrain gravity diameter')
-    .populate('starships')
-    .populate('species')
-    .populate({
+  const { producer } = req.query;
+  if(producer){
+    const producerFilter = new RegExp(req.query.producer, 'i');
+    Film
+      .find()
+      .where('producer')
+      .regex(producerFilter)
+      .sort( { episode: 1 } )
+      .select('episode')
+      .populate('characters', 'name gender height skin_color hair_color eye_color')
+      .populate('planets', '-_id name climate terrain gravity diameter')
+      .populate('starships')
+      .populate('species')
+      .populate({
         path: 'vehicles',
         populate: { path: 'pilots'}
       })
-    .then(films => {
-      res.json(films);
-    })
-    .catch(err => res.status(500).json({ error: 'Error reading the DB' }));
+      .then(films => {
+        res.json(films);
+      })
+      .catch(err => res.status(500).json({ error: 'Error reading the DB' }));
+  }
+  else
+    Film
+      .find()
+      .sort( { episode: 1 } )
+      .select('episode')
+      .populate('characters', 'name gender height skin_color hair_color eye_color')
+      .populate('planets', '-_id name climate terrain gravity diameter')
+      .populate('starships')
+      .populate('species')
+      .populate({
+        path: 'vehicles',
+        populate: { path: 'pilots'}
+      })
+      .then(films => {
+        res.json(films);
+      })
+      .catch(err => res.status(500).json({ error: 'Error reading the DB' }));
 })
 
 module.exports = router;
