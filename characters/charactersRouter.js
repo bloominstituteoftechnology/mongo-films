8f6e46066db68fb5a4deef792963b9df299db263
A99 <A99@99s-macbook-pro.home>
2018-06-07 02:15:00 +0200

    GET functionality for Characters & Connected Models

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -4,6 +4,20 @@ const Character = require('./Character.js');
 
 const router = express.Router();
 
-// add endpoints here
+router.route('/').get((req, res) => {
+    Character.find()
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json({ error: err.message }));
+})
+
+router.route('/:id').get((req, res) => {
+    const { id } = req.params;
+    Character.findById(id).populate('homeworld', '-_id -edited -created -__v -key')
+        .then(response => {
+            if(response === null) res.status(400).json({ error: `The character by that ID does not exist.`});
+            res.json(response);
+        })
+        .catch(err => res.status(500).json({ error: err.message }));
+})
 
 module.exports = router;
