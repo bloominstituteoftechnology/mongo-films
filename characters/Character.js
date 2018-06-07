60ae1dab5e935d2b56cc684a2e60c23beabe2977
Jacob Henry <jacobiswinning@gmail.com>
2018-06-06 23:54:39 -0600

    added all fields for reference

--- characters/Character.js
+++ characters/Character.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const Character = mongoose.Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true
+  },
   edited: Date,
   created: Date,
   gender: String,
@@ -11,9 +14,16 @@ const Character = mongoose.Schema({
   skin_color: String,
   eye_color: String,
   birth_year: String,
-  key: { type: Number, unique: true },
+  key: {
+    type: Number,
+    unique: true
+  },
   homeworld_key: Number,
   // add homeworld field that links the character to it's planet
+  homeworld: {
+    type: ObjectId,
+    ref: 'planet'
+  }
 });
 
 module.exports = mongoose.model('Character', Character);
--- vehicles/Vehicle.js
+++ vehicles/Vehicle.js
@@ -4,8 +4,15 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const Vehicle = new mongoose.Schema({
   vehicle_class: String,
   pilot_keys: [Number],
-  key: { type: Number, unique: true },
+  key: {
+    type: Number,
+    unique: true
+  },
   // add pilots field to link it to the Character model
+  pilots: {
+    type: ObjectId,
+    ref: 'Character'
+  }
 });
 
 module.exports = mongoose.model('Vehicle', Vehicle);
