d77ffed03f245c2583c19eab501d6b7a946cef25
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-06-07 12:10:25 -0700

    added code from demo

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -8,15 +8,34 @@ const router = express.Router();
 router
     .route('/')
     .get((req, res) => {
-        Film
-            .find()
-            .sort('episode')
-            .then(response => {
-                res.status(200).json(response)
-            })
-            .catch(error => {
-                res.status(500).json({ error: error.message })
-            })
+        const { producer } = req.query;
+        if(producer) {
+            const producerFilter = new RegExp(producer, 'i')
+            Film
+                .find({})
+                //.find({producer: { $regex: producer, $options: 'i'} })
+                .where('producer')
+                .regex(producerFilter)
+                .then(response => {
+                    res.status(200).json(response)
+                })
+                .catch(error => {
+                    res.status(500).json({ error: error.message })
+                })
+        } else {
+            Film
+                .find({})  
+                .sort('episode')
+                // .select('episode title director producer')  
+                .populate('characters','_id name gender height skin_color hair_color eye_color')
+                .populate('planets', 'name climate terrain gravity diameter')
+                .then(response => {
+                    res.status(200).json(response)
+                })
+                .catch(error => {
+                    res.status(500).json({ error: error.message })
+                })
+        }
     })
     
 
