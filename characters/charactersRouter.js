17533b4ea106130ae5eace8d091c3d056ec72e55
David <AquilaVirtual>
2018-06-07 15:03:03 -0700

    Endpoints implementation for producer query, released_date, and female character taller than 100cm

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,16 +5,22 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
-
-router.route('/')
-.get((req, res) => {
-Character.find()
-.then(characters => {
-  res.status(200).json(characters)
-})
-.catch(err => {
-    res.status(500).json({errorMessage: `The Characters information could not be retrieved.error: ${err}`});
-    })
+router
+.route('/')
+	.get((req, res) => {
+		const { minheight }  = req.query;
+		let query = Character.find({})
+		if (minheight) {
+			Character.find({ gender: 'female', height: {$exists: true}, $where: `this.height >= 100`})
+			.then(character => { 
+        console.log(character);
+        res.status(200).json(character)
+        return;
+			})
+			.catch(err => {
+				res.status(500).json({ error: 'There was an error retrieving the data' })
+			})
+		}
 })
 
 module.exports = router;
