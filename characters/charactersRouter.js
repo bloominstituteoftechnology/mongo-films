2cb60cca5658b4d1857c46814c9b377fd8686dd4
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-07 14:37:29 -0400

    - added fields to GET /characters and GET /characters/:id - added GET /characters/:id/vehicle endpoint

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Character = require('./Character.js');
+const Vehicle = require('../vehicles/Vehicle');
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ const router = express.Router();
 router
     .route('/')
     .get((req, res) => {
-        Character.find({}, {'_id': 1, 'name': 1, 'gender': 1, 'height': 1, 'skin_colpr': 1, 'hair_color': 1, 'eye_color': 1})
+        Character.find({}, {'_id': 1, 'name': 1, 'gender': 1, 'height': 1, 'skin_color': 1, 'hair_color': 1, 'eye_color': 1})
         .then(characters => res.json(characters))
         .catch(err => res.status(500).json({error: err}))
     })
@@ -19,9 +20,20 @@ router
     .get((req, res) => {
         const { id }  = req.params;
         Character.findById(id)
-
+        .select('name gender height skin_color hair_color eye_color')
         .populate('Planet')
         .then(foundCharacter => res.json(foundCharacter))
+        .catch(err => res.status(500).json({error: err}))
+    })
+
+router
+    .route('/:id/vehicles')
+    .get((req, res) => {
+        console.log(req.params)
+        const {id} = req.params;
+        Vehicle.findById(id)
+        .select('Character')
+        .then(foundVehicle => res.json(foundVehicle))
         .catch(err => res.status(500).json({error: err}))
     })
 
