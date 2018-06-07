6ef94f4a57ff1f4833745cac4600261d234d8090
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-07 02:25:09 -0500

    implemented GET for films

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,4 +6,12 @@ const router = express.Router();
 
 // add endpoints here
 
+router
+    .route('/')
+    .get((req, res) => {
+        Film.find({})
+            .then(films => res.json(films))
+            .catch(err => res.status(500).json({ error: err.message}));
+    })
+
 module.exports = router;
