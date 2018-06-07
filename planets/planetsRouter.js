cf8a4c1ad3f2a2030eeaacd13838aa0a12bd8365
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-08 02:59:12 +0700

    added get chars by planet they were born on

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,9 +1,30 @@
 const express = require('express');
 
 const Planet = require('./Planet.js');
+const Character = require('../characters/Character');
+const Specie = require('../species/Specie');
 
 const router = express.Router();
 
 // add endpoints here
+router
+  .route('/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    Planet.findById(id)
+      .then(planet => {
+        Character.find({ homeworld_key: planet.key })
+          .select('name gender height skin_color hair_color eye_color')  
+          .then(chars => {
+            res.status(200).json(chars);
+          })
+          .catch(err => {
+            res.status(500).json([{ err: error.message }])
+          })
+      })
+      .catch(err => {
+        res.status(500).json([{ err: error.message }])
+      })
+  })
 
 module.exports = router;
