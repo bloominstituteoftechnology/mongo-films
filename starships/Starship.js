87e35b3271a2ec7d2cb0394f303562413747e8b6
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-07 03:47:42 -0500

    implemented GET by ID for starships Also selectively populated ref data

--- starships/Starship.js
+++ starships/Starship.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
+const Schema = mongoose.Schema;
 
-const Starship = new mongoose.Schema({
+const Starship = new Schema({
   pilot_keys: [Number],
   mglt: String,
   starship_class: String,
