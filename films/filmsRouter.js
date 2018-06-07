13cba55ad83a208bbc250d5920eb6c9a1e112bc1
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-07 14:17:32 -0700

    get request for films
    - sort by episode
    - populate characters
    - populate planets

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,6 +4,25 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
-// add endpoints here
+router
+    .route('/')
+    .get((req, res) => {
+        Film.find()
+            .sort('episode')
+            .populate(
+                'characters',
+                '_id name gender height skin_color hair_color eye_color'
+            )
+            .populate(
+                'planets',
+                'name climate terrain gravity diameter'
+            )
+            .then( films => {
+                res.json(films)
+            })
+            .catch( err => {
+                res.status(500).json(err.message)
+            })
+    })
 
 module.exports = router;
