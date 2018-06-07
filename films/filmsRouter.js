c4b90cf3cc7cc1c229d75abab4da83a6f7053e08
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-06-07 17:18:45 -0400

    films find query released and mixed queries

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -3,52 +3,44 @@ const express = require('express');
 const Film = require('./Film.js');
 
 const router = express.Router();
-
-
 // add endpoints here
 
 router
 .route('/')
 .get((req, res) => {
-  const { producer } = req.query;
+  const { producer, released } = req.query;
+
+  let filmQuery = Film
+    .find();
   if(producer){
     const producerFilter = new RegExp(req.query.producer, 'i');
-    Film
-      .find()
+    filmQuery
       .where('producer')
-      .regex(producerFilter)
-      .sort( { episode: 1 } )
-      .select('episode')
-      .populate('characters', 'name gender height skin_color hair_color eye_color')
-      .populate('planets', '-_id name climate terrain gravity diameter')
-      .populate('starships')
-      .populate('species')
-      .populate({
-        path: 'vehicles',
-        populate: { path: 'pilots'}
-      })
-      .then(films => {
-        res.json(films);
-      })
-      .catch(err => res.status(500).json({ error: 'Error reading the DB' }));
+      .regex(producerFilter);
   }
-  else
-    Film
-      .find()
-      .sort( { episode: 1 } )
-      .select('episode')
-      .populate('characters', 'name gender height skin_color hair_color eye_color')
-      .populate('planets', '-_id name climate terrain gravity diameter')
-      .populate('starships')
-      .populate('species')
-      .populate({
-        path: 'vehicles',
-        populate: { path: 'pilots'}
-      })
-      .then(films => {
-        res.json(films);
-      })
-      .catch(err => res.status(500).json({ error: 'Error reading the DB' }));
+  if(released){
+    const releasedFilter = new RegExp(req.query.released, 'i');
+    filmQuery
+      .where('release_date')
+      .regex(releasedFilter);
+  }
+  filmQuery
+    .sort( { episode: 1 } )
+    .select('-_id episode producer release_date')
+    .populate('characters', 'name gender height skin_color hair_color eye_color')
+    .populate('planets', '-_id name climate terrain gravity diameter')
+    .populate('starships')
+    .populate('species')
+    .populate({
+      path: 'vehicles',
+      populate: { path: 'pilots'}
+    })
+    .then(films => 
+      res.json(films)
+    )
+    .catch(err => 
+      res.status(500).json({ error: 'Error reading the DB' })
+    )
 })
 
 module.exports = router;
