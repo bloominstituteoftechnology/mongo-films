438537c69014fc6ee16b5fc82265634bcb6f4af2
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-07 15:14:59 -0500

    added producer & release date queries following instructor notes

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -9,19 +9,42 @@ const router = express.Router();
 router
     .route('/')
     .get((req, res) => {
-        Film.find({})
+        const { producer, release_date } = req.query;
+
+        if(producer) {
+            const producerFilter = new RegExp(producer, 'i'); // build a case-insensitive match around the producer name
+            Film.find() // look at all documents in collection
+                .where('producer') // find where producer matches the following regex pattern
+                .regex(producerFilter) // pass our regex pattern to the regex func
+                .populate('characters', ('name gender height skin_color hair_color eye_color'))
+                .populate('planets', ('name climate terrain gravity diameter -_id'))
+                .sort('episode')
+                .then(films => res.json(films))
+                .catch(err => res.status(500).json({ error: err.message }))
+        
+        } else if (release_date) {
+            const releaseDateFilter = new RegExp(release_date, 'i'); 
+            Film.find() 
+                .where('release_date') 
+                .regex(releaseDateFilter) 
+                .populate('characters', ('name gender height skin_color hair_color eye_color'))
+                .populate('planets', ('name climate terrain gravity diameter -_id'))
+                .sort('episode')
+                .then(films => res.json(films))
+                .catch(err => res.status(500).json({ error: err.message }))
+
+        } else {
+            Film.find({})
             .populate('characters', ('name gender height skin_color hair_color eye_color'))
             .populate('planets', ('name climate terrain gravity diameter -_id'))
-            .populate('vehicles')
-            .populate('starships')
-            .populate('species')
             .sort('episode')
             .then(films => res.json(films))
             .catch(err => res.status(500).json({ error: err.message}));
+        }
     })
-    .post((req, res) => {
-        const film = ({ created, edited, title})
-    })
+
+    
+
 router  
     .route('/:id')
     .get((req, res) => {
@@ -29,9 +52,6 @@ router
         Film.findById(id)
         .populate('characters', ('name gender height skin_color hair_color eye_color'))
         .populate('planets', ('name climate terrain gravity diameter -_id'))
-        .populate('vehicles')
-        .populate('starships')
-        .populate('species')
             .then(film => res.json(film))
             .catch(err => res.status(500).json({ error: err.message}));
     })
