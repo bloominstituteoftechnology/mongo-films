8b6ada1b7789bae76eaefb66e1493ea540806f80
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-07 16:26:32 -0700

    get species and characters for a planet

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,9 +1,51 @@
-const express = require('express');
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Types.ObjectId
+const express = require('express')
+const Planet = require('./Planet.js')
+const Character = require('../characters/Character.js')
+const Specie = require('../species/Specie.js')
+const router = express.Router()
 
-const Planet = require('./Planet.js');
+router
+    .route('/')
+    .get((req, res) => {
+        Planet.find()
+            .then(planets => {
+                res.json(planets)
+            })
+            .catch( err => {
+                res.status(500).json({ error: err.message })
+            })
+    })
 
-const router = express.Router();
-
-// add endpoints here
-
-module.exports = router;
+router
+    .route('/:id')
+    .get((req, res) => {
+        const { id } = req.params
+        Planet.find({ _id: { $eq: ObjectId(id) } })
+            .then( planet => {
+                let charsAndSpecies = {}
+                Character.find({ 'homeworld': id })
+                    .then( chars => {
+                        Specie.find({ 'homeworld': id })
+                            .then( species => {
+                                charsAndSpecies = {
+                                    ...charsAndSpecies, 
+                                    characters: chars, 
+                                    species: species}
+                                res.json(charsAndSpecies)
+                            })
+                            .catch( err => {
+                                res.status(500).json({ error: err.message })
+                            })
+                    })
+                    .catch( err => {
+                        res.status(500).json({ error: err.message })
+                    })
+            })
+            .catch( err => {
+                res.status(500).json({ error: err.message })
+            })
+    })
+    
+module.exports = router
