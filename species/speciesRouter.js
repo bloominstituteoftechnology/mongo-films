4d0143864d35808d60edae9c9d1494dc521ba19c
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-07 03:37:56 -0500

    implemented GET by ID for species also selectively populated ref data and corrected typo in GET

--- species/speciesRouter.js
+++ species/speciesRouter.js
@@ -7,11 +7,21 @@ const router = express.Router();
 // add endpoints here
 
 router
-    .route('./')
+    .route('/')
     .get((req, res) => {
         Specie.find()
             .then(species => res.json(species))
             .catch(err => res.status(500).json({ error: err.message }));
+    })
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        Specie.findById(id)
+            .populate('homeworld', {_id: 0, climate: 1, name: 1, terrain: 1})
+            .then(specie => res.json(specie))
+            .catch(err => res.status(500).json({ error: err.message}));
     })
 
 module.exports = router;
