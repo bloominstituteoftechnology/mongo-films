cd4b61a10ee7f54f06b9453b572cc799e4cee17f
Glenn-David Daniel <d2rd@outlook.com>
2018-06-06 23:02:28 -0700

    WIP: organized to implement characterRouter requirments.

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -7,6 +7,7 @@ const router = express.Router()
 // add endpoints here
 router.get('/', (req, res) => {
   Character.find()
+    .populate('name', 'gender height eye_color hair_color')
     .then(chars => res.send(200).json({chars: chars}))
     .catch(err => res.status(500).json(err))
 })
@@ -34,4 +35,16 @@ router.get('/:id', (req, res) => {
     .catch(err => res.status(200).json(err))
 })
 */
+
+// Return specific results for a character
+// ☞ 99041f27-1518-47b7-8ad1-0a9814172482
+
+// Find all vehicles driven by a given character. (/api/characters/:id/vehicles)
+
+
+// Find all female characters taller than 100cm (/api/characters?minheight=100)
+
+// Given a planet Id find all `characters` born in that planet and all native `species`. (/api/planet/:id)
+
+
 module.exports = router
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,15 +5,20 @@ const Film = require('./Film.js')
 const router = express.Router()
 
 // add endpoints here
+// ☞ c73e5146-e6db-481d-af2a-4f4d0b266e54
+
 router.get('/', (req, res) => {
   Film.find()
-    .sort('-episode')
+    .sort('episode')
     .select('episode title planets characters')
+    .populate('characters', '_id name gender height skin_color hair_color eye_color') // you can chain populate as long as subsequent populates reference different models.
     .populate('planets', 'name climate terrain gravity diameter')
-    .populate('characters', 'name') // you can chain populate as long as subsequent populates reference different models.
-    .where().gt()
+    // .where().gt()
     .then(films => res.status(200).json(films))
 })
+/*
+//Find Films by producer
+// ☞ f40185c8-0030-48ea-b1fd-cb0e2e10e685
 router.get('/', (req, res) => {
   const { producer, released } = req.query
   let query = Film.find()
@@ -31,4 +36,9 @@ router.get('/', (req, res) => {
   query.then(films => res.status(200).json(films))
     .catch(err => res.status(500).json(err))
 })
+*/
+
+// film released in a specific year
+// ☞ d529d380-6494-4b17-bf3d-d2af8fe61083
+
 module.exports = router
