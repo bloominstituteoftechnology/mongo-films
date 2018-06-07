cc628801ee4e67c31a13aaf68ee9c65e4a106ca8
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-07 15:55:37 -0700

    get vehicles for a character

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -3,6 +3,7 @@ const ObjectId = mongoose.Types.ObjectId
 const express = require('express')
 const Character = require('./Character.js')
 const Film = require('../films/Film.js')
+const Vehicle = require('../vehicles/Vehicle.js')
 const router = express.Router()
 
 router
@@ -28,7 +29,8 @@ router
             )
             .then( char => {
                 Film.find({characters: id})
-                    .select('title')
+                    .select('title episode')
+                    .sort('episode')
                     .then( films => {
                         const newChar = {...char, movies: films}
                         res.json(newChar)
@@ -39,4 +41,23 @@ router
             })
     })
 
+router
+    .route('/:id/vehicles')
+    .get((req, res) => {
+        const { id } = req.params
+        Character.find({_id: {$eq: ObjectId(id)}})
+            .populate(
+                'homeworld',
+                'name climate terrain gravity diameter'
+            )
+            .then( char => {
+                Vehicle.find({pilots: id})
+                    .then( vehicles => {
+                        res.json(vehicles)
+                    })
+                    .catch( err => {
+                        res.status(500).json({ error: err.message })
+                    })
+            })
+    })
 module.exports = router
