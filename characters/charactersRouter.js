c6912e8b7975a4782f5e89b983e5604f13b65814
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-07 12:17:25 +0700

    added get and get by id for characters

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -6,16 +6,33 @@ const router = express.Router();
 
 // add endpoints here
 
-// router
-//   .route('/')
-//   .get((req, res) => {
-//     Character.find()
-//       .then(chars => {
-//         res.status(200).json(chars);
-//       })
-//       .catch(err => {
-//         res.status(500).json([{ error: "The character information could not be retrieved." }]);
-//       });
-//   })
+router
+  .route('/')
+  .get((req, res) => {
+    Character.find()
+      .populate('homeworld')
+      .then(chars => {
+        res.status(200).json(chars);
+      })
+      .catch(err => {
+        res.status(500).json([{ error: "The character information could not be retrieved." }]);
+      });
+  });
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    Character.findById(id)
+      .populate('homeworld')
+      .then(chars => {
+        res.status(200).json(chars);
+      })
+      .catch(err => {
+        res.status(500).json([{ error: err }]);
+      });
+  });
+
+
 
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -11,8 +11,8 @@ router
     if (req.query.producers) {
       Film.find()
         .where({ producer: new RegExp(req.query.producers, "gi")})  
-        .populate("characters", { edited: 0, created: 0, birth_year: 0, key: 0, homeworld_key: 0, homeworld: 0, __v: 0 })
-        .populate("planets", { edited: 0, surface_water: 0, rotation_period: 0, created: 0, orbital_period: 0, key: 0, __v: 0, _id: 0 })
+        .populate("characters", ('name gender height skin_color hair_color eye_color'))
+        .populate("planets", ('name climate terrain gravity diameter -_id'))
         .then(films => {
           res.status(200).json(films);
         })
@@ -23,7 +23,7 @@ router
       Film.find()
         .where({ release_date: new RegExp(req.query.dates, "gi")})
         .populate("characters", { edited: 0, created: 0, birth_year: 0, key: 0, homeworld_key: 0, homeworld: 0, __v: 0 })
-        .populate("planets", { edited: 0, surface_water: 0, rotation_period: 0, created: 0, orbital_period: 0, key: 0, __v: 0, _id: 0 })
+        .populate("planets", ('name climate terrain gravity diameter -_id'))
         .then(films => {
           res.status(200).json(films);
         })
@@ -34,7 +34,7 @@ router
       Film.find()
         .sort("episode")  
         .populate("characters", { edited: 0, created: 0, birth_year: 0, key: 0, homeworld_key: 0, homeworld: 0, __v: 0 })
-        .populate("planets", { edited: 0, surface_water: 0, rotation_period: 0, created: 0, orbital_period: 0, key: 0, __v: 0, _id: 0 })
+        .populate("planets", ('name climate terrain gravity diameter -_id'))
         .then(films => {
           res.status(200).json(films);
         })
