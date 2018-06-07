0daba050b970c3c661d3c8588e8a3782f3fd0780
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-07 14:42:03 -0700

    get films with queries
    - by release date
    - by producer

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -7,7 +7,50 @@ const router = express.Router();
 router
     .route('/')
     .get((req, res) => {
-        Film.find()
+        const { producer, released } = req.query;
+        if (producer) {
+            const producerFilter = new RegExp(producer, 'i');
+            Film.find()
+                .where('producer')
+                .regex(producerFilter)
+                .sort('episode')
+                .populate(
+                    'characters',
+                    '_id name gender height skin_color hair_color eye_color'
+                )
+                .populate(
+                    'planets',
+                    'name climate terrain gravity diameter'
+                )
+                .then( films => {
+                    res.json(films)
+                })
+                .catch( err => {
+                    res.status(500).json({ error: err.message })
+                })
+                
+        } else if (released) {
+            const releasedFilter = new RegExp(released, 'i');
+            Film.find()
+                .where('release_date')
+                .regex(releasedFilter)
+                .sort('episode')
+                .populate(
+                    'characters',
+                    '_id name gender height skin_color hair_color eye_color'
+                )
+                .populate(
+                    'planets',
+                    'name climate terrain gravity diameter'
+                )
+                .then( films => {
+                    res.json(films)
+                })
+                .catch( err => {
+                    res.status(500).json({ error: err.message })
+                })
+        } else {
+            Film.find()
             .sort('episode')
             .populate(
                 'characters',
@@ -21,8 +64,9 @@ router
                 res.json(films)
             })
             .catch( err => {
-                res.status(500).json(err.message)
+                res.status(500).json({ error: err.message })
             })
+        }
     })
 
 module.exports = router;
