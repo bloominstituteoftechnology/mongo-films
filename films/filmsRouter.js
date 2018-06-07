96c5c4aef790500eeac8d9d6078433c131b52c95
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-07 12:07:19 -0400

    - Added GET /films and GET film/:id and added to schema

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,26 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router
+    .route('/')
+    .get((req, res) => {
+        Film.find({})
+        .then(films => res.json(films))
+        .catch(err => res.status(500).json({error: err}))
+    })
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        const { id }  = req.params;
+        Film.findById(id)
+        .populate('Planet')
+        .populate('Starship')
+        .populate('Character')
+        .populate('Vehicle')
+        .populate('Specie')
+        .then(foundFilm => res.json(foundFilm))
+        .catch(err => res.status(500).json({error: err}))
+    })
 
 module.exports = router;
