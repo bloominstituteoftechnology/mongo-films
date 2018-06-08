1811c4936139e5d9c94d4f74663d11acbf1ce335
Frank Faustino <nawzyah@gmail.com>
2018-06-07 22:15:08 -0700

    Refactored character and films routes

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -3,31 +3,27 @@ const Character = require('./Character')
 const Film = require('../films/Film')
 const Vehicle = require('../vehicles/Vehicle')
 
-router.get('/', async (req, res) => {
-  const { minheight } = req.query
-  try {
-    const response = await Character.find({ gender: 'female' })
-      .where('height')
-      .gt(100)
-    res.json(response)
-  } catch (err) {
-    res.status(500).json({ err: err.message })
+router.get('/', (req, res) => {
+  const { gender, minheight } = req.query
+  const query = Character.find()
+  if (gender) query.where({ gender })
+  if (minheight !== undefined && !isNaN(minheight)) {
+    query.where('height').gt(Number(minheight))
   }
+  query
+    .then(response => res.json(response))
+    .catch(err => res.status(500).json(err.message))
 })
 
-router.get('/:id?', async (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params
   try {
-    if (id) {
-      const charByID = await Character.findById(id)
-        .populate('homeworld')
-        .select('name gender skin_color hair_color height eye_color birth_year')
-      const movies = await Film.where({ characters: id })
-        .select('title producer director episode release_date')
-      res.json({ ...charByID._doc, movies })
-    }
-    const characters = await Character.find({})
-    res.json(characters)
+    const charByID = await Character.findById(id)
+      .populate('homeworld', '-_id name')
+      .select('-_id name gender skin_color hair_color height eye_color birth_year')
+    const movies = await Film.where({ characters: id })
+      .select('-_id title producer director episode release_date')
+    res.json({ ...charByID._doc, movies })
   } catch (err) {
     res.status(500).json({ err: err.message })
   }
@@ -36,7 +32,7 @@ router.get('/:id?', async (req, res) => {
 router.get('/:id/vehicles', async (req, res) => {
   try {
     const { id } = req.params
-    const response = await Vehicle.find({ pilots: [id] })
+    const response = await Vehicle.find({ pilots: id })
       .populate('pilots', 'name')
       .select('vehicle_class')
     res.json(response)
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -2,20 +2,27 @@ const router = require('express').Router()
 const Film = require('./Film')
 
 router.get('/', (req, res) => {
-  try {
-    const { producer, released } = req.query
-    const films = Film.find({})
-      .sort('episode')
-      .select('title producer release_date')
-      .populate('characters', 'name gender height skin_color hair_color eye_color')
-      .populate('planets', 'name climate terrain gravity diameter')
+  const { producer, released } = req.query
+  const films = Film.find({})
+    .sort('episode')
+    .select('title producer release_date')
+    .populate('characters', 'name gender height skin_color hair_color eye_color')
+    .populate('planets', 'name climate terrain gravity diameter')
 
-    if (producer) films.where({ producer: new RegExp(producer, 'i') })
-    if (released) films.where({ release_date: new RegExp(released, 'i') })
-    films.then(film => res.json(film))
+  if (producer) films.where({ producer: new RegExp(producer, 'i') })
+  if (released) films.where({ release_date: new RegExp(released) })
+  films
+    .then(film => res.json(film))
+    .catch(err => res.status(500).json({ err: err.message }))
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const film = await Film.findById(req.params.id)
+    res.json(film)
   } catch (err) {
     res.status(500).json({ err: err.message })
   }
 })
 
-module.exports = router;
+module.exports = router
