96afba2ee663309905519d926e51fc0d993d4793
JPEmery <clearkeyemery@gmail.com>
2018-06-07 21:46:03 -0500

    JP Emery: gender and minheight complete

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -6,38 +6,49 @@ const Movies = require('../films/Film.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    Character.find()
-    .then((character) => {
-        res.status(200).json(character);
+
+    const { minheight } = req.query;
+    let query = Character.find();
+
+    if(minheight) {
+        query.find({gender: 'female'}).where('height').gt(minheight);
+    }
+    query.then(characters => {
+        res.status(200).json(characters);
+    })
+    .catch(error => {
+        res.status(500).json({error: 'information could not be retrieved'})
     })
   // need to imput gender check and height check
-  
+
 
 
 })
 .get('/:id', (req, res) => {
     const { id } = req.params;
-    Character.findById(id)
-    .populate('homeworld', 'name').select('name')
-    .then(char => {
-        res.status(200).json(char)
-    })
-    .catch(err => {
-    res.status(500).json({error: 'character with id does not exist'})
-    })
-})
-.get('/:id/movies', (req, res) => {
-    const { id } = req.params;
-    Movies.find()
-    .populate('characters', 'episode name').select('name')
 
-    .then(character => {
-        res.status(200).json(character)
-    }) 
-    .catch(err => {
-        res.status(500).json({error: 'information could not be retrived'});
+    const Movies2 = Movies.find()
+        .where({ characters: id})
+        .select('episode title release_date director producer')
+        .sort('episode')
+    
+    const character2 = Character.find()
+    .where({'_id': id})
+    .populate('homeworld');
+    // .then(char => {
+    //     res.status(200).json(char)
+    // })
+    // .catch(err => {
+    // res.status(500).json({error: 'character with id does not exist'})
+    // })
+
+    Promise.all([character2, Movies2])
+    .then(joined  => {
+        const [ _movies_, characters ] = joined;
+        res.status(200).json({characters, _movies_});
     })
 })
+
 
 
 .get('/:id/vehicle',(req, res) => {
