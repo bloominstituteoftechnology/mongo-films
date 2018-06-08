57479fea54fa9eb6380debbe0772ace9d64a7b63
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-07 20:25:52 -0400

    Created a helper method in util that will format incoming request query strings - It also has config options to allow/disallow certain paths to be included in the returning query string

--- util/formatRequestQuery.js
+++ util/formatRequestQuery.js
@@ -1,16 +1,27 @@
-// {producer: {$regex: 'gary kurtz' , $options: 'i'}}
+const ALLOWED_FILTERS = {
+  Film: {
+    producer: 'producer',
+    director: 'director',
+    released: 'release_date'
+  }
+};
 
-const getFilters = (query) => {
+const formatRequestQuery = (model, query) => {
   const keys = Object.keys(query);
   const values = Object.values(query);
   const filters = {};
 
   keys.forEach((x, i) => {
-    filters[x] = {
-      $regex: values[i],
-      $options: 'i'
+    let path = ALLOWED_FILTERS[model][x];
+    if(path){
+      filters[path] = {
+        $regex: values[i],
+        $options: 'i'
+      }
     }
   });
 
   return filters;
 }
+
+module.exports = formatRequestQuery;
