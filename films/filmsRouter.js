2d712123dc03c6a7cae41655e5e2fb3247f3bbe6
maribelcuales <maribelcuales@gmail.com>
2018-06-11 09:44:43 +0800

    added fields to Film.js and Get method to filmsRouter

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -1,32 +1,35 @@
 const express = require('express');
-
 const Film = require('./Film.js');
-
+const Character = require('../characters/Character.js')
 const router = express.Router();
 
 // add endpoints here
-router.route('/').get((req, res) => {
-    const { producer } = req.query; 
+
+router.get ('/', function (req, res) {
+    let query = Film.find()
+        .select('producer release_date episode title director')
+        .populate('characters', 'name gender height skin_color hair_color eye_color _id')
+        .populate('planets', 'name climate terrain gravity diameter -_id'); 
+
+    const { producer, released } = req.query; 
+    //Order by episode 1-6 
+    query.sort('episode')
+
+    // Find all films produced by Gary Kurtz
+    // Postman test okay. http://localhost:5000/api/films?producer=gary+kurtz    
+
     if (producer) {
-        const producerFilter = new RegExp(producer, 'i');
-        console.log(producerFilter);
-        Film.find({})
-            .where('producer')
-            .regex(producerFilter)
-            .then(films => res.json(films))
-            .catch(error => res.status(500).json({ error: error.message }));         
-    } else {
-        Film.find({})
-            .sort('episode')
-            // .select ('episode title director producer')
-            .populate(
-                'characters',
-                '_id name gender height skin_color hair_color eye_color'
-            )
-            .populate('planets', 'name climate terrain gravity diameter')
-            .then(films => res.json(films))
-            .catch(error => res.status(500).json({ error: error.message })); 
+        const filter = new RegExp(producer, 'i'); 
+        query.where({ producer: filter })
     }
-}); 
+
+    // Find all films released in 2005
+    // Postman test okay. http://localhost:5000/api/films?released=2005
+
+    if (release) {
+        query.where({ release_date: {$regex: released, $options: 'i'} });
+    }
+    query.then(films => res.status(200).json(films)); 
+});
 
 module.exports = router;
