9e106b8d40cf23dedd970989ef1d6acd20e16e92
Bee C <b.choeung@gmail.com>
2018-06-13 17:16:21 -0400

    successfully did a GET for all vehicles

--- vehicles/vehiclesRouter.js
+++ vehicles/vehiclesRouter.js
@@ -5,5 +5,20 @@ const Vehicle = require('./Vehicle.js');
 const router = express.Router();
 
 // add endpoints here
+//GET all characters
+router
+.route( '/' )
+.get( ( req, res ) =>
+{
+Vehicle.find()
+    .then( vehicles =>
+    {
+    res.status( 200 ).json( vehicles );
+    } )
+    .catch( err =>
+    {
+    res.status( 500 ).json( { error: 'Error' } )
+    } );
+} )
 
 module.exports = router;
