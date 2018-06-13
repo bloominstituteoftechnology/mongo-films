2216fb8f3f453759586bf0457a6970de2db69bb5
Bee C <b.choeung@gmail.com>
2018-06-13 17:11:16 -0400

    successfully did a GET for all species

--- species/speciesRouter.js
+++ species/speciesRouter.js
@@ -5,5 +5,20 @@ const Specie = require('./Specie.js');
 const router = express.Router();
 
 // add endpoints here
+//GET all characters
+router
+.route( '/' )
+.get( ( req, res ) =>
+{
+Specie.find()
+    .then( species =>
+    {
+    res.status( 200 ).json( species );
+    } )
+    .catch( err =>
+    {
+    res.status( 500 ).json( { error: 'Error' } )
+    } );
+} )
 
 module.exports = router;
