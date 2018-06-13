c9ba9abcb51811e96bf49abaa4c1005262ff3507
CMVNK <Kopecky12112@gmail.com>
2018-06-13 13:44:57 -0700

    created some endpoints and investigated the json docs to see what
    one of the students was talking about in regards to an error
    
    I have the same error but I don't think it will affect anything
    
    Says End of file expected at the beginning of every second entry

--- characters/Character.js
+++ characters/Character.js
@@ -2,16 +2,40 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const Character = mongoose.Schema({
-  name: { type: String, required: true },
-  edited: Date,
-  created: Date,
-  gender: String,
-  height: String,
-  hair_color: String,
-  skin_color: String,
-  eye_color: String,
-  birth_year: String,
-  key: { type: Number, unique: true },
+  name: { 
+    type: String, 
+    required: true 
+  },
+  edited: {
+    type: Date,
+    default: Date.now()
+  },
+  created: {
+    type: Date,
+    default: Date.now()
+  },
+  gender: {
+    type: String,
+  },
+  height: {
+    type: Number,
+  },
+  hair_color: {
+    type:String,
+  },
+  skin_color: {
+    type:String,
+  },
+  eye_color: {
+    type: String,
+  },
+  birth_year: {
+    type: String,
+  },
+  key: { 
+    type: Number, 
+    unique: true 
+  },
   homeworld_key: Number,
   // add homeworld field that links the character to it's planet
 });
--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -4,6 +4,69 @@ const Character = require('./Character.js');
 
 const router = express.Router();
 
+const sendUserError = (status, message, res, err="Not from Catch") =>{
+    res.status(status).json({Error: message, err});
+    return;
+}
+
 // add endpoints here
+const get = (req, res) =>{
+    Character.find()
+        .then(characters =>{
+            res.status(200).json(characters)
+        })
+        .catch(err =>{
+            sendUserError(500, `There was an error in retrieving characters`, res, err);
+        });
+}
+
+const post = (req, res) =>{
+    const { name, gender, height, hair_color, skin_color, eye_color, birth_year } = req.body;
+    const characterTraits = { name, gender, height, hair_color, skin_color, eye_color, birth_year };
+        const character = new Character(characterTraits)
+
+    character
+        .save()
+        .then(character =>{
+            res.status(201).json(character);
+        })
+        .catch(err =>{
+            sendUserError(500, "There was in error in saving bear to database", res, err)
+        });
+}
+
+const getId = (req, res) =>{
+    const { id } = req.params;
+
+    Character.findById(id)
+        .then(character =>{
+            if(character.length===0){
+                sendUserError(404, `There was an error in fetching character with ${id}`, res);
+            }
+            res.status(200).json(character);
+        })
+        .catch(err =>{
+            sendUserError(500, "There was an error in fetching character");
+        });
+}
+
+const deleteId = (req, res) =>{
+    const { id } = req.params;
+
+    Character
+        .remove({ _id:id })
+        .then(result =>{
+            res.status(204).json(`Success: Character with ${id} was deleted successfully from database.`);
+        })
+        .catch(err =>{
+            sendUserError(500, "There was an error in removing character from database.", res, err);
+        });
+}
+
+const updateId = (req, res) =>{
+    const { id } = req.params;
+    const { name, gender, height, hair_color, skin_color, eye_color, birth_year } = req.body;
+    const characterTraits = { name, gender, height, hair_color, skin_color, eye_color, birth_year };
+}
 
 module.exports = router;
