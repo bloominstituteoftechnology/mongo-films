4e8116fe227b8f4675a9f1e5c1f3911f45786696
Bee C <b.choeung@gmail.com>
2018-06-13 17:09:21 -0400

    successfully did a GET for all planets

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -5,5 +5,20 @@ const Planet = require('./Planet.js');
 const router = express.Router();
 
 // add endpoints here
+//GET all characters
+router
+.route( '/' )
+.get( ( req, res ) =>
+{
+Planet.find()
+    .then( planets =>
+    {
+    res.status( 200 ).json( planets );
+    } )
+    .catch( err =>
+    {
+    res.status( 500 ).json( { error: 'Error' } )
+    } );
+} )
 
 module.exports = router;
