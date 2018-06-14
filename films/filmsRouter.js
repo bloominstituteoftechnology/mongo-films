7fd3ef08fbef2a3dddce9774643aa927d459ab39
JSNavey <jsnavey@gmail.com>
2018-06-14 14:47:56 -0400

    implement regex to find films produced by Gary Kurtz

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -14,18 +14,31 @@ const errorMessage = (status, message, res) => {
 router
     .route('/')
     .get((req, res) => {
-        Film
-            .find({})
-            .sort({ episode: 1 })
-            // .select('episode title director producer') //select (act like projection in compass) to show only these catagory.
-            .populate('characters', '_id name gender height skin_color hair_color eye_color')
-            .populate('planets', 'name climate terrain gravity diameter')
-            .then(films => {
-                res.status(200).json({ films })
-            })
-            .catch(error => {
-                errorMessage(500, 'Something wrong to get the data', res)
-            })
+        // console.log(req.query); 
+        const { producer } = req.query;
+        if(producer) {
+            Film
+                .find({ producer: { $regex: producer, $options: 'i' } })
+                .then(films => {
+                    res.status(200).json({ films })
+                })
+                .catch(error => {
+                    errorMessage(500, 'Something wrong with our data', res)
+                })
+        } else {
+            Film
+                .find({})
+                .sort({ episode: 1 })
+                // .select('episode title director producer') //select (act like projection in compass) to show only these catagory.
+                .populate('characters', '_id name gender height skin_color hair_color eye_color')
+                .populate('planets', 'name climate terrain gravity diameter')
+                .then(films => {
+                    res.status(200).json({ films })
+                })
+                .catch(error => {
+                    errorMessage(500, 'Something wrong with our data', res)
+                })
+            }
 })
 
 module.exports = router;
