584aec40806639e3440e96f09a304012e3d5b60d
FireInjun <Fireinjun291@gmail.com>
2018-06-14 19:38:39 -0400

    added routes
    additional as required

--- routers/charactersRouter.js
+++ routers/charactersRouter.js
@@ -4,23 +4,36 @@ const Character = require('../schemas/Character.js');
 
 const router = express.Router();
 
-// add endpoints here
-// ! All characters!!!!
 router
     .route( '/' )
     .get( ( req, res ) =>
     {
-        Character
-            .find()
-            .then( characters =>
+        Character.find()
+            .then( character =>
             {
-                res.status( 200 ).json( characters );
+                res.status( 200 ).json( character );
             } )
             .catch( err =>
             {
-                res.status( 500 ).json( { error: 'Error' } )
+                res.status( 500 ).json( { error: 'err' } )
             } );
     } )
-    
+
+    .post( ( req, res ) =>
+    {
+        const { name, gender, height, skin_color, hair_color, eye_color } = req.body;
+        const newChar = new Character( { name, gender, height, skin_color, hair_color, eye_color } );
+        newChar
+            .save()
+            .then( addedChar =>
+            {
+                res.status( 201 ).json( addedChar );
+            } )
+            .catch( err =>
+            {
+                res.status( 422 ).json( { error: 'err' } );
+            } )
+    } )
+
 
 module.exports = router;
--- routers/filmsRouter.js
+++ routers/filmsRouter.js
@@ -15,7 +15,7 @@ router
             .sort( 'episode' )
             .select( 'episode title director producer' )
             .populate( 'characters', '_id name gender height skin_color hair_color eye_color' )
-            .populate( 'planets name climate terrain gravity diameter' )
+            .populate( 'planets', 'name climate terrain gravity diameter' )
             .then( films =>
             {
                 res.status( 200 ).json( films );
