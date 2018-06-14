dc979f5a8841595caa7f4baa5c1e113cd6ed238a
JSNavey <jsnavey@gmail.com>
2018-06-14 16:47:00 -0400

    filter the released year

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -16,16 +16,32 @@ router
     .get((req, res) => {
         // console.log(req.query); 
         const { producer } = req.query;
+        const { released } = req.query;
         if(producer) {
             Film
-                .find({ producer: { $regex: producer, $options: 'i' } })
+                .find({ producer: { $regex: producer, $options: 'i' } }) //i for case sensitive
                 .then(films => {
                     res.status(200).json({ films })
                 })
                 .catch(error => {
                     errorMessage(500, 'Something wrong with our data', res)
                 })
-        } else {
+        } 
+        if(released) {  // req.query using regex can use like above and below, come up with the same outcome.
+            const yearFilter = new RegExp(released);  
+            Film
+                .find({})
+                .where('release_date') //where to find the matched year in data file or compass 
+                .regex(yearFilter)
+                .then(films => {
+                    res.status(200).json({ films })
+                })
+                .catch(error => {
+                    errorMessage(500, 'Something wrong with our data', res)
+                })
+        }
+        
+        else {
             Film
                 .find({})
                 .sort({ episode: 1 })
