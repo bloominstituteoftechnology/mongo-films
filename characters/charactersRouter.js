bbeebb930f1f8ca47c8d14f1a87268b2db62b1af
april <martinezapril56@yahoo.com>
2018-06-14 15:59:13 -0400

    got our populate

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -17,42 +17,82 @@ router
             {
                 res.status( 500 ).json( { error: 'Error' } )
             } );
-    } ) 
+    } )
     .post( ( req, res ) =>
     {
         const { name } = req.body;
         const newCharacter = new Character( { name } );
         newCharacter
-            .save() // Returns as a promise
+            .save()
             .then( savedcharacters =>
             {
-                res.status( 201 ).json( savedcharacters );
+                res.status( 201 ).json( savecharacters );
             } )
             .catch( error =>
             {
-                res.status( 400 ).json( { errorMessage: "Please provide Name  for the characters." } );
+                res.status( 400 ).json( { errorMessage: "Please provide firstName, lastName and age for the friend." } )
             } )
-        // res.status(201).json({ status: 'please implement POST functionality' });
-    } )
-
-
-    
+    } );
+router
     .route( '/:id' )
     .get( ( req, res ) =>
     {
         const { id } = req.params;
         Character.findById( id )
-            .then( character =>
+            .then( foundcharacters =>
             {
-                res.status( 200 ).json( characters );
+                res.status( 200 ).json( foundcharacters );
             } )
             .catch( err =>
             {
                 res.status( 500 ).json( { errorMessage: "The friends information could not be retrieved." } );
             } )
     } )
+    .put( ( req, res ) =>
+    {
+        const { id } = req.params;
+        const updates = ( { name } = req.body );
+        // findByIdAndUpdate
+        Character
+            .findByIdAndUpdate( id, updates, { name } = req.body )
+            .then( characters =>
+            {
+                res.json( characters );
+            } )
+            .catch( err =>
+            {
+                res.status( 500 ).json( { status: 'error didnt find what your looking for' } );
+            } )
+
+            .delete( ( req, res ) =>
+            {
+                const { id } = req.params;
+                const updates = ( {  } = req.body );
+                // findByIdAndUpdate
+                Character
+                    .findByIdAndRemove( id, updates, { name } = req.body )
+                    .then( characterRemoved =>
+                    {
+                        res.json( characterRemoved );
+                    } )
+                    .catch( err =>
+                    {
+                        res.status( 500 ).json( { status: 'error didnt find what your looking for' } );
+                    } )
+                // res.json(200).json({ status: 'please implement PUT functionality' });
+            } )
+        // res.json(200).json({ status: 'please implement PUT functionality' });
+    } )
+// router.route("/")
+//     .get(get)
+//     .post(post);
+
+// router.route("/:id")
+//     .get(getId)
+//     .delete(deleteId)
+//     .put(updateId);
+
+// router.route("/:id/planet")
+//     .post(postPlanet)
     
-
-
-
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -1,9 +1,51 @@
-const express = require('express');
+const express = require( 'express' );
 
-const Film = require('./Film.js');
+const Film = require( './Film.js' );
 
 const router = express.Router();
 
+
 // add endpoints here
+//GET all characters
+router
+    .route( '/' )
+    .get( ( req, res ) =>
+    {
+        Film.find()
+            .then( films =>
+            {
+                res.status( 200 ).json( films );
+            } )
+            .catch( err =>
+            {
+                res.status( 500 ).json( { error: 'Error' } )
+            } );
+    } )
+
+// add endpoints here
+//GET all characters
+router
+    .route( '/films' )
+    .get( ( req, res ) =>
+    {
+        Film.find( {} )
+            .sort( { episode: 1 } )
+            .select( 'episode' )
+            .populate( 'characters',
+                '_id, name, gender, height, skin_color, hair_color and eye_color'
+            )
+            // .populate('characters',
+            // '_id, name, gender, height, skin_color, hair_color and eye_color'
+            // )
+            .then( films =>
+            {
+                res.status( 200 ).json( films );
+            } )
+            .catch( err =>
+            {
+                res.status( 500 ).json( { error: 'Error' } )
+            } );
+    } )
+
 
 module.exports = router;
