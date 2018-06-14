16fd74d6a3fd93618bae628fdf60cdb2151f085e
Bee C <b.choeung@gmail.com>
2018-06-14 18:11:35 -0400

    successfully added query all films produced by Gary Kurtz (/api/films?producer=gary+kurtz)

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,57 +6,27 @@ const router = express.Router();
 
 
 // add endpoints here
-//GET all characters
-router
-.route( '/' )
-.get( ( req, res ) =>
-{
-Film.find()
-    .sort({ episode: 1})
-    .select('episode')
-    .populate('characters',
-    '_id name gender height skin_color hair_color and eye_color'
-    )
-    .populate('planets',
-    'name climate terrain gravity and diameter'
-    )
-    //2nd method to POPULATE
-    // .populate({path: 'characters', select: '-_id, name, gender, height, skin_color, hair_color and eye_color'})
-    .then( films =>
-    {
-    res.status( 200 ).json( films );
-    } )
-    .catch( err =>
-    {
-    res.status( 500 ).json( { error: 'Error' } )
-    } );
-} )
+router.route( '/' ).get(( req, res ) => {
+    const { producer } = req.query;
+    if (producer) {
+        Film.find({ producer: { $regex: producer, $options: 'i'} })
+            .then(films => res.json(films))
+            .catch(error => res.status(500).json({ error: error.message }));
+    } else {
+        Film.find({})
+            .sort({ episode: 1})
+            .select('episode')
+            .populate('characters',
+            '_id name gender height skin_color hair_color and eye_color'
+            )
+            .populate('planets',
+            'name climate terrain gravity and diameter'
+            )
+            //2nd method to POPULATE
+            // .populate({path: 'characters', select: '-_id, name, gender, height, skin_color, hair_color and eye_color'})
+            .then( films => res.json( films ))
+            .catch( error => res.status( 500 ).json({ error: error.message }));
+    } 
+});
 
 module.exports = router;
-
-
-// add endpoints here
-//GET all characters
-// router
-// .route( '/films' )
-// .get( ( req, res ) =>
-// {
-// Film.find({})
-    // .sort({ episode: 1})
-    // .select('episode')
-    // .populate('characters',
-    // '_id, name, gender, height, skin_color, hair_color and eye_color'
-    // )
-    // .populate(
-    //     'planets',
-    //     'name, climate, terrain, gravity and diameter'
-    // )
-//     .then( films =>
-//     {
-//     res.status( 200 ).json( films );
-//     } )
-//     .catch( err =>
-//     {
-//     res.status( 500 ).json( { error: 'Error' } )
-//     } );
-// } )
