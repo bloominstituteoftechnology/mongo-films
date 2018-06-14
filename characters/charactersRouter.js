abb21d02357ccb9df195f29a59bfb4c7668abb79
JSNavey <jsnavey@gmail.com>
2018-06-14 17:25:23 -0400

    implemented female minheight

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -13,32 +13,42 @@ const errorMessage = (status, message, res) => {
 router
     .route('/')
     .get((req, res) => {
-        Character
-            .find()
-            .then(char => {
-                res.status(200).json({ char })
-            })
-            .catch(error => {
-                errorMessage(500, 'Something wrong to get the data', res)
-            })
+        const { id } = req.params;    
+        const { minheight} = req.query;
+        if(minheight) {
+            Character
+                .find({ height: { $gt: minheight}, gender: 'female' }) //gt = greater than
+                .then(char => {
+                    res.status(200).json({ char })
+                })
+        } else {
+            Character
+                .find()
+                .then(char => {
+                    res.status(200).json({ char })
+                })
+                .catch(error => {
+                    errorMessage(500, 'Something wrong to get the data', res)
+                })
+            }
 })
 
 
 router
     .route('/:id')
     .get((req, res) => {
-        const { id } = req.params;        
-        Character
-            .findById(id)
-            .then(foundChar => {
-                if(!foundChar) {
-                    errorMessage(404, 'No the specific character found', res);
-                }
-                res.status(200).json({ foundChar })
-            })
-            .catch(error => {
-                errorMessage(500, error, res)
-            })
+            
+            Character
+                .findById(id)            
+                .then(foundChar => {
+                    if(!foundChar) {
+                        errorMessage(404, 'No the specific character found', res);
+                    }
+                    res.status(200).json({ foundChar })
+                })
+                .catch(error => {
+                    errorMessage(500, error, res)
+                })
 })
 
 module.exports = router;
