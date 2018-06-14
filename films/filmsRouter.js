537964c995d8a3d0772d519f99055cb2e2dc850a
Bee C <b.choeung@gmail.com>
2018-06-14 15:59:42 -0400

    added order, populate character information

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -4,6 +4,7 @@ const Film = require('./Film.js');
 
 const router = express.Router();
 
+
 // add endpoints here
 //GET all characters
 router
@@ -11,6 +12,31 @@ router
 .get( ( req, res ) =>
 {
 Film.find()
+    .then( films =>
+    {
+    res.status( 200 ).json( films );
+    } )
+    .catch( err =>
+    {
+    res.status( 500 ).json( { error: 'Error' } )
+    } );
+} )
+
+// add endpoints here
+//GET all characters
+router
+.route( '/films' )
+.get( ( req, res ) =>
+{
+Film.find({})
+    .sort({ episode: 1})
+    .select('episode')
+    .populate('characters',
+    '_id, name, gender, height, skin_color, hair_color and eye_color'
+    )
+    // .populate('characters',
+    // '_id, name, gender, height, skin_color, hair_color and eye_color'
+    // )
     .then( films =>
     {
     res.status( 200 ).json( films );
