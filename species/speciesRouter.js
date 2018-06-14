c90b882a83cc5c8cea16f26525c31ba18d142f93
Tramane Hall <tramanedhall@gmail.com>
2018-06-14 16:02:15 -0400

    Finished Endpoints for all databases

--- species/speciesRouter.js
+++ species/speciesRouter.js
@@ -4,6 +4,38 @@ const Specie = require('./Specie.js');
 
 const router = express.Router();
 
-// add endpoints here
+router
+  .route('/')
+  .get((req, res) => {
+
+    Specie
+    .find()
+    .then(species => {
+      res.status(200)
+      res.json({ species })
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({ message: 'The species information could not be retrieved.' });
+    })
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    const { id } = req.params; 
+
+    Specie
+    .findById(id)
+    .populate('homeworld')
+    .then(specie => {
+        res.status(200)
+        res.json({ specie })
+    })
+    .catch(err => {
+        res.status(500)
+        res.json({ message: "The specie information could not be retrieved." })
+    })
+  })
 
 module.exports = router;
--- starships/starshipsRouter.js
+++ starships/starshipsRouter.js
@@ -4,6 +4,39 @@ const Starship = require('./Starship.js');
 
 const router = express.Router();
 
-// add endpoints here
+router
+  .route('/')
+  .get((req, res) => {
+
+    Starship
+    .find()
+    .then(starships => {
+      res.status(200)
+      res.json({ starships })
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({ message: 'The starships information could not be retrieved.' });
+    })
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    const { id } = req.params; 
+
+    Starship
+    .findById(id)
+    .populate('pilots')
+    .then(starship => {
+        res.status(200)
+        res.json({ starship })
+    })
+    .catch(err => {
+        res.status(500)
+        res.json({ message: "The starship information could not be retrieved." })
+    })
+  })
+
 
 module.exports = router;
